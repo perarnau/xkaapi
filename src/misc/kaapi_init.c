b76f875fde0340a6d8c7544306d3d0ceef3b3974
Fabien Lementec <lementec@idkoiff.imag.fr>
2009-12-02 17:43:30 +0100

    [update] parse KAAPI_CPUSET environment variable as described in the grammar. function comments.

--- src/misc/kaapi_init.c
+++ src/misc/kaapi_init.c
@@ -46,9 +46,11 @@
 #include "kaapi_impl.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <unistd.h>
+#include <errno.h>
 
 
 /*
@@ -76,76 +78,301 @@ kaapi_format_t kaapi_double_format;
 
 
 /** cpuset related routines
-*/
+ -- grammar
+    cpu_set   : cpu_expr ( ',' cpu_expr ) *
+    cpu_expr  : '!'* cpu_list
+    cpu_list  : cpu_index ':' cpu_index |
+                ':' cpu_index |
+                ':'
+    cpu_index : num
+    num       : [ '0' '9' ]+
+ */
+
+struct cpuset_parser
+{
+#define CPU_BIT_IS_DISABLED (1 << 0)
+#define CPU_BIT_IS_USABLE (1 << 1)
+  unsigned char cpu_bits[KAAPI_MAX_PROCESSOR];
+
+  unsigned int* kid_map;
+
+  unsigned int used_ncpus;
+  unsigned int sys_ncpus;
+  unsigned int total_ncpus;
+
+  const char* str_pos;
+  int err_no;
+};
+
+
+typedef struct cpuset_parser cpuset_parser_t;
+
+
+static void init_parser
+(
+ cpuset_parser_t* parser,
+ unsigned int* kid_map,
+ unsigned int sys_ncpus,
+ unsigned int total_ncpus,
+ const char* str_pos
+)
+{
+  memset(parser, 0, sizeof(cpuset_parser_t));
+
+  parser->kid_map = kid_map;
+
+  parser->total_ncpus = total_ncpus;
+  parser->sys_ncpus = sys_ncpus;
+
+  parser->str_pos = str_pos;
+}
+
+
+static inline void set_parser_error
+(
+ cpuset_parser_t* parser,
+ int err_no
+)
+{
+  parser->err_no = err_no;
+}
+
+
+static inline int parse_cpu_index
+(
+ cpuset_parser_t* parser,
+ unsigned int* index
+)
+{
+  char* end_pos;
+
+  *index = (unsigned int)strtoul(parser->str_pos, &end_pos, 10);
+
+  if (end_pos == parser->str_pos)
+    {
+      set_parser_error(parser, EINVAL);
+      return -1;
+    }
+
+  if (*index >= parser->sys_ncpus)
+    {
+      set_parser_error(parser, E2BIG);
+      return -1;
+    }
+
+  parser->str_pos = end_pos;
+
+  return 0;
+}
+
+
+static inline int is_digit(char c)
+{
+  return (c >= '0') && (c <= '9');
+}
+
+
+static inline int is_list_delim(char c)
+{
+  return c == ':';
+}
+
+
+static inline int is_eol(char c)
+{
+  /* end of list */
+  return (c == ',') || (c == 0);
+}
+
+
+static int parse_cpu_list
+(
+ cpuset_parser_t* parser,
+ unsigned int* index_low,
+ unsigned int* index_high
+)
+{
+  /* 1 token look ahead */
+  if (is_digit(*parser->str_pos))
+    {
+      if (parse_cpu_index(parser, index_low))
+	return -1;
+    }
+  else
+    {
+      *index_low = 0;
+    }
+
+  *index_high = *index_low;
+
+  if (is_list_delim(*parser->str_pos))
+    {
+      ++parser->str_pos;
+
+      /* 1 token look ahead */
+      if (is_eol(*parser->str_pos))
+	{
+	  *index_high = parser->total_ncpus - 1;
+	}
+      else if (parse_cpu_index(parser, index_high))
+	{
+	  return -1;
+	}
+    }
+
+  /* swap indices if needed */
+  if (*index_low > *index_high)
+    {
+      const unsigned int tmp_index = *index_high;
+      *index_high = *index_low;
+      *index_low = tmp_index;
+    }
+
+  return 0;
+}
+
+
+static int parse_cpu_expr(cpuset_parser_t* parser)
+{
+  unsigned int is_enabled = 1;
+  unsigned int index_low;
+  unsigned int index_high;
+
+  for (; *parser->str_pos == '!'; ++parser->str_pos)
+    is_enabled ^= 1;
+
+  if (parse_cpu_list(parser, &index_low, &index_high))
+    return -1;
+
+  for (; index_low <= index_high; ++index_low)
+    {
+      if (is_enabled == 0)
+	parser->cpu_bits[index_low] |= CPU_BIT_IS_DISABLED;
+
+      if (parser->cpu_bits[index_low] & CPU_BIT_IS_DISABLED)
+	{
+	  /* this cpu is disabled. if it has been previously
+	     mark usable, discard it and decrement the cpu cuont
+	  */
+
+	  if (parser->cpu_bits[index_low] & CPU_BIT_IS_USABLE)
+	    {
+	      parser->cpu_bits[index_low] &= ~CPU_BIT_IS_USABLE;
+	      --parser->used_ncpus;
+	    }
+	}
+      else if (!(parser->cpu_bits[index_low] & CPU_BIT_IS_USABLE))
+	{
+	  /* cpu not previously usable */
+
+	  parser->cpu_bits[index_low] |= CPU_BIT_IS_USABLE;
+
+	  if (++parser->used_ncpus == parser->total_ncpus)
+	    break;
+	}
+    }
+
+  return 0;
+}
+
+
+static int parse_cpu_set(cpuset_parser_t* parser)
+{
+  unsigned int i;
+  unsigned int j;
+
+  /* build the cpu set from string */
+
+  while (1)
+    {
+      if (parse_cpu_expr(parser))
+	return -1;
+
+      if (*parser->str_pos != ',')
+	{
+	  if (!*parser->str_pos)
+	    break;
+
+	  set_parser_error(parser, EINVAL);
+
+	  return -1;
+	}
+
+      ++parser->str_pos;
+    }
+
+  /* bind kid to available cpus */
+
+  for (j = 0, i = 0; i < parser->used_ncpus; ++i, ++j)
+    {
+      for (; !(parser->cpu_bits[j] & CPU_BIT_IS_USABLE); ++j)
+	;
+
+      parser->kid_map[i] = j;
+    }
+
+  return 0;
+}
+
+
+#if 0
+static void __attribute__((unused))
+print_parser(const cpuset_parser_t* parser)
+{
+  unsigned int i;
+
+  for (i = 0; i < parser->used_ncpus; ++i)
+    printf("%u ~ %u\n", i, parser->kid_map[i]);
+}
+#endif
+
+
 static void fill_identity_kid_map
     (
      unsigned int* kid_map,
-     unsigned long ncpus
+     unsigned int ncpus
     )
 {
-  unsigned long icpu;
+  unsigned int icpu;
 
   for (icpu = 0; icpu < ncpus; ++icpu)
-    kid_map[icpu] = (unsigned int)icpu;
+    kid_map[icpu] = icpu;
 }
 
 
-static unsigned long str_to_kid_map
+static int str_to_kid_map
     (
      unsigned int* kid_map,
-     const char* s,
+     const char* cpuset_str,
      unsigned int sys_ncpus,
-     unsigned int total_ncpus
+     unsigned int* total_ncpus
     )
 {
-  /* s contains a comma separated
-   list of cpus indices to use
-   */
+  cpuset_parser_t parser;
 
-  unsigned char used_map[KAAPI_MAX_PROCESSOR];
-  unsigned long icpu;
-  unsigned int used_ncpus;
-  unsigned int is_done = 0;
-  char* e;
+  if (cpuset_str == NULL)
+    return 0;
 
-  for (icpu = 0; icpu < KAAPI_MAX_PROCESSOR; ++icpu)
-    used_map[icpu] = 0;
-  
-  used_ncpus = 0;
+  init_parser(&parser, kid_map, sys_ncpus,
+	      *total_ncpus, cpuset_str);
 
-  while (!is_done)
-  {
-    icpu = strtoul(s, &e, 10);
+  if (parse_cpu_set(&parser))
+    return parser.err_no;
 
-    if (icpu >= sys_ncpus)
-      {
-	printf("KAAPI_CPUSET, cpu index too high\n");
-	abort();
-      }
+  *total_ncpus = parser.used_ncpus;
 
-    if (*e != ',')
-      is_done = 1;
-
-    s = e + 1;
-
-    if (used_map[icpu])
-      continue ;
-
-    used_map[icpu] = 1;
-    
-    kid_map[used_ncpus++] = icpu;
-
-    if (used_ncpus >= total_ncpus)
-      is_done = 1;
-  }
-  
-  /* return the acutal used cpu count */
-  
-  return used_ncpus;
+  return 0;
 }
 
 
-/**
+/** \ingroup WS
+    Initialize from xkaapi runtime parameters from command line
+    \param argc [IN] command line argument count
+    \param argv [IN] command line argument vector
+    \retval 0 in case of success 
+    \retval EINVAL because of error when parsing then KAAPI_CPUSET string
+    \retval E2BIG because of a cpu index too high in KAAPI_CPUSET
+    
 */
 extern int kaapi_setup_param( int argc, char** argv )
 {
@@ -189,11 +416,14 @@ extern int kaapi_setup_param( int argc, char** argv )
 
   if (getenv("KAAPI_CPUSET") != 0)
   {
-    default_param.cpucount =
+    int err_no =
       str_to_kid_map(default_param.kid_to_cpu,
 		     getenv("KAAPI_CPUSET"),
 		     default_param.syscpucount,
-		     default_param.cpucount);
+		     &default_param.cpucount);
+
+    if (err_no)
+      return err_no;
 
     default_param.use_affinity = 1;
   }
