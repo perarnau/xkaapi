1b60899a6c317dcde3a05dbc5fc4a3aa308ea4db
Fabien Lementec <lementec@idkoiff.imag.fr>
2009-12-03 11:38:28 +0100

    [add] add kastl to build system. port accumulate, transfrom. factorize some code in kaapi_utils.

---
+++ kastl.wip/accumulate.h
@@ -0,0 +1,251 @@
+/*
+ *  test_accumulate.cpp
+ *  xkaapi
+ *
+ *  Created by DT on fevrier 2009.
+ *  Created by FLM on december 2009.
+ *  Copyright 2009 INRIA. All rights reserved.
+ *
+ */
+#ifndef _CKAAPI_ACCUMULATE_H
+#define _CKAAPI_ACCUMULATE_H
+#include "kaapi.h"
+#include "kaapi_utils.h"
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+template<class RandomAccessIterator, class T>
+ T  accumulate ( RandomAccessIterator begin, RandomAccessIterator end,
+                 T init);
+
+/** Stucture of a work for accumulate
+*/
+template<class RandomAccessIterator, class T, class BinOp>
+class AccumulateStruct {
+public:
+  typedef AccumulateStruct<RandomAccessIterator, T, BinOp> Self_t;
+
+  /* cstor */
+  AccumulateStruct(
+    RandomAccessIterator ibeg,
+    RandomAccessIterator iend,
+    T init,
+    BinOp op,
+    bool is_master = true) : _ibeg(ibeg), _iend(iend), _local_accumulate(init),
+                             _op(op), _is_master(is_master) 
+  {}
+  
+  /* do accumulate */
+  void doit(kaapi_task_t* task, kaapi_stack_t* stack);
+
+  /* get result */
+  T get_accumulate() {
+     return _local_accumulate;
+  }
+ 
+  RandomAccessIterator  _ibeg;
+  RandomAccessIterator  _iend;
+  T _local_accumulate; 
+  BinOp _op;
+  bool _is_master; 
+
+  // request handler
+  struct request_handler
+  {
+    RandomAccessIterator local_end;
+    size_t bloc;
+    BinOp op;
+
+    request_handler(RandomAccessIterator& _local_end, size_t _bloc, BinOp _op)
+      : local_end(_local_end), bloc(_bloc), op(_op)
+    {}
+
+    bool operator() (Self_t* self_work, Self_t* output_work)
+    {
+      output_work->_iend = local_end;
+      output_work->_ibeg = local_end-bloc;
+      local_end -= bloc;
+      output_work->_local_accumulate = 0;
+      output_work->_op = op;
+      output_work->_is_master = false;
+
+      kaapi_assert_debug( output_work->_iend - output_work->_ibeg > 0);
+
+      return true;
+    }
+  };
+
+  typedef struct request_handler request_handler_t;
+
+  /** splitter_work is called within the context of the steal point
+  */
+  int splitter( kaapi_stack_t* victim_stack, kaapi_task_t* task,
+		int count, kaapi_request_t* request )
+  {
+    size_t size = (_iend - _ibeg);
+    const int total_count = count;
+    int replied_count = 0;
+    size_t bloc;
+
+    /* threshold should be defined (...) */
+    if (size < 512)
+      goto finish_splitter;
+    
+    bloc = size / (1 + count);
+
+    if (bloc < 128) { count = size / 128 - 1; bloc = 128; }
+
+    // iterate over requests
+    {
+      request_handler_t handler(_iend, bloc, _op);
+
+      replied_count =
+	kaapi_utils::foreach_request
+	(
+	 victim_stack, task,
+	 count, request,
+	 handler, this
+	 );
+
+      // mute victim state after processing
+      _iend = handler.local_end;
+
+      kaapi_assert_debug(_iend - _ibeg > 0);
+    }
+
+  finish_splitter:
+    {
+      // fail the remaining requests
+
+      const int remaining_count = total_count - replied_count;
+
+      if (remaining_count)
+	{
+	  kaapi_utils::fail_requests
+	    (
+	     victim_stack,
+	     task,
+	     remaining_count,
+	     request + replied_count
+	     );
+	}
+    }
+
+    // all requests have been replied to
+    return total_count;
+  }
+
+
+#if 0 // TODO_REDUCER
+  /* Called by the victim thread to collect work from one other thread
+  */
+  static void reducer( kaapi_steal_context_t* sc, void* thief_data, 
+                       AccumulateStruct<RandomAccessIterator, T, BinOp>* victim_data )
+  {
+    AccumulateStruct<RandomAccessIterator, T, BinOp>* thief_work = 
+      (AccumulateStruct<RandomAccessIterator, T, BinOp>* )thief_data;
+
+    AccumulateStruct<RandomAccessIterator, T, BinOp>* victim_work =
+      (AccumulateStruct<RandomAccessIterator, T, BinOp>* )victim_data;
+
+
+   //merge of the two results
+   if(thief_work->_local_accumulate!=T(0))    
+     victim_work->_local_accumulate  = victim_work->_op(victim_work->_local_accumulate,
+                                                        thief_work->_local_accumulate);
+
+    if (thief_work->_ibeg != thief_work->_iend)
+    {
+#if defined(SEQ_SUBCOUNT)
+      victim_work->_local_accumulate = std::accumulate(thief_work->_ibeg, thief_work->_iend, 
+                                        victim_work->__local_accumulate, victim_work->_op);
+#else
+
+      AccumulateStruct<RandomAccessIterator, T, BinOp> 
+        work( sc, 
+              thief_work->_ibeg, 
+              thief_work->_iend, 
+              victim_work->_local_accumulate,
+              thief_work->_op 
+            );
+      work.doit();
+
+      victim_work->_local_accumulate = work._local_accumulate;
+#endif
+    }
+  }
+#endif // TODO_REDUCER
+};
+
+
+/** Adaptive accumulate
+*/
+template<class RandomAccessIterator, class T, class BinOp>
+void AccumulateStruct<RandomAccessIterator, T, BinOp>::doit(kaapi_task_t* task, kaapi_stack_t* stack)
+{
+  /* local iterator for the nano loop */
+  RandomAccessIterator nano_iend;
+  
+  /* amount of work per iteration of the nano loop */
+  ptrdiff_t unit_size = 512;
+
+  ptrdiff_t tmp_size = 0;
+
+  while (_iend != _ibeg)
+  {
+    /* definition of the steal point where steal_work may be called in case of steal request 
+       -here size is pass as parameter and updated in case of steal.
+    */
+    kaapi_stealpoint( stack, task, &kaapi_utils::static_splitter<Self_t> );
+
+    tmp_size = _iend-_ibeg;
+    if(tmp_size < unit_size ) {
+       unit_size = tmp_size; nano_iend = _iend;
+    } else {
+       nano_iend = _ibeg + unit_size;
+    }
+    
+    /* sequential computation */
+    if(_is_master) _local_accumulate = std::accumulate(_ibeg, nano_iend, _local_accumulate, _op);
+    else {
+     RandomAccessIterator first = _ibeg;
+     RandomAccessIterator last  = nano_iend;
+     T init = *first++;
+         while ( first!=last ) init = _op(init, *first++);
+         _local_accumulate = (_local_accumulate==T(0))?init: _op(_local_accumulate, init);
+    }
+    _ibeg +=unit_size;
+  }
+
+  /* definition of the finalization point where all stolen work a interrupt and collected */
+  kaapi_finalize_steal( stack, task );
+
+#warning "TODO_REDUCER"
+
+  /* Here the thiefs have finish the computation and returns their inits which have been reduced using reducer function. */  
+}
+
+
+/**
+*/
+template<class RandomAccessIterator, class T >
+T accumulate(RandomAccessIterator begin, RandomAccessIterator end, T init)
+{
+  AccumulateStruct<RandomAccessIterator, T, std::plus<T> > work( begin, end, init, std::plus<T>() );
+  kaapi_stack_t* const stack = kaapi_self_stack();
+
+  /* will receive & process steal request */
+  kaapi_task_t* const task = kaapi_stack_toptask(stack);
+  kaapi_task_init(stack, task, KAAPI_TASK_ADAPTIVE);
+  kaapi_task_setargs(task, &work);
+  kaapi_stack_pushtask(stack);
+
+  work.doit(task, stack);
+
+  kaapi_stack_poptask(stack);
+
+  return work.get_accumulate();
+}
+
+#endif
---
+++ kastl.wip/kaapi_utils.h
@@ -0,0 +1,106 @@
+/*
+ *  transform.cpp
+ *  xkaapi
+ *
+ *  Created by FLM
+ *  Copyright 2009 INRIA. All rights reserved.
+ *
+ */
+
+
+#ifndef _XKAAPI_KAAPI_UTILS_H
+#define _XKAAPI_KAAPI_UTILS_H
+
+
+
+#include "kaapi.h"
+
+
+
+// TODO: should be in kaapi.h
+static inline void kaapi_stack_popdata(kaapi_stack_t* stack, kaapi_uint32_t count)
+{
+  stack->sp_data -= count;
+}
+
+
+
+namespace kaapi_utils
+{
+  // prototypes
+
+  void fail_requests(kaapi_stack_t*, kaapi_task_t*, int, kaapi_request_t*);
+
+  // static function definistions
+
+  template<typename SelfType>
+  static void static_thiefentrypoint( kaapi_task_t* task, kaapi_stack_t* stack )
+  {
+    SelfType* self_work = kaapi_task_getargst(task, SelfType);
+    self_work->doit(task, stack);
+  }
+
+  template<typename SelfType>
+  static int static_splitter( kaapi_stack_t* victim_stack, kaapi_task_t* task, int count, kaapi_request_t* request )
+  {
+    SelfType* const self_work = kaapi_task_getargst(task, SelfType);
+    return self_work->splitter( victim_stack, task, count, request );
+  }
+
+  template<typename SelfType>
+  static int foreach_request
+  (
+   kaapi_stack_t* victim_stack,
+   kaapi_task_t* task,
+   int count,
+   kaapi_request_t* request,
+   typename SelfType::request_handler_t& handler,
+   SelfType* this_work
+   )
+  {
+    // process the requests and fail
+    // the one we did not reply to
+
+    const int replied_count = count;
+    SelfType* output_work = 0;
+
+    for (; count > 0; ++request)
+      {
+	if (!kaapi_request_ok(request))
+	  continue ;
+
+	kaapi_stack_t* const thief_stack = request->stack;
+	kaapi_task_t*  const thief_task  = kaapi_stack_toptask(thief_stack);
+	void* const stack_data = kaapi_stack_pushdata(thief_stack, sizeof(SelfType));
+
+	kaapi_task_init(thief_stack, thief_task, KAAPI_TASK_ADAPTIVE);
+	kaapi_task_setbody(thief_task, &static_thiefentrypoint<SelfType>);
+	kaapi_task_setargs(thief_task, stack_data);
+
+	output_work = kaapi_task_getargst(thief_task, SelfType);
+
+	// stop request processing
+	if (handler(this_work, output_work) == false)
+	  {
+	    kaapi_stack_popdata(thief_stack, sizeof(SelfType));
+	    break;
+	  }
+
+	kaapi_stack_pushtask(thief_stack);
+
+	// reply ok (1) to the request
+	kaapi_request_reply(victim_stack, task, request, thief_stack, 1);
+
+	--count; 
+      }
+
+    fail_requests(victim_stack, task, count, request);
+
+    return replied_count;
+  }
+
+} // kaapi_utils namespace
+
+
+
+#endif // ! _XKAAPI_KAAPI_UTILS_H
