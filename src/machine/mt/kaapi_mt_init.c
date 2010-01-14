dbc8183d4cb140b17cf690779fd0a71453b00d68
Thierry Gautier <Thierry.Gautier@imag.fr>
2010-01-14 13:16:48 +0100

    Add timing output in case of PERFCOUNTER

--- src/machine/mt/kaapi_mt_init.c
+++ src/machine/mt/kaapi_mt_init.c
@@ -131,7 +131,12 @@ void __attribute__ ((constructor)) kaapi_init(void)
   kaapi_stack_pushretn(stack, &frame);
   
   /* dump output information */
+#if defined(KAAPI_USE_PERFCOUNTER)
+  printf("[KAAPI::INIT] use #physical cpu:%u, start time:%15f\n", default_param.cpucount,kaapi_get_elapsedtime());
+#else
   printf("[KAAPI::INIT] use #physical cpu:%u\n", default_param.cpucount);
+#endif
+  fflush( stdout );
 }
 
 
@@ -144,9 +149,14 @@ void __attribute__ ((destructor)) kaapi_fini(void)
   kaapi_uint64_t cnt_stealreqok;
   kaapi_uint64_t cnt_stealreq;
   kaapi_uint64_t cnt_stealop;
+  kaapi_uint64_t cnt_suspend;
   double t_idle;
   
+#if defined(KAAPI_USE_PERFCOUNTER)
+  printf("[KAAPI::TERM] end time:%15f\n", kaapi_get_elapsedtime());
+#else
   printf("[KAAPI::TERM]\n");
+#endif
   fflush( stdout );
 
   /* wait end of the initialization */
@@ -162,6 +172,7 @@ void __attribute__ ((destructor)) kaapi_fini(void)
   cnt_stealreqok  = 0;
   cnt_stealreq    = 0;
   cnt_stealop     = 0;
+  cnt_suspend     = 0;
   t_idle          = 0;
   for (i=0; i<kaapi_count_kprocessors; ++i)
   {
@@ -169,7 +180,23 @@ void __attribute__ ((destructor)) kaapi_fini(void)
     cnt_stealreqok  += kaapi_all_kprocessors[i]->cnt_stealreqok;
     cnt_stealreq    += kaapi_all_kprocessors[i]->cnt_stealreq;
     cnt_stealop     += kaapi_all_kprocessors[i]->cnt_stealop;
+    cnt_suspend     += kaapi_all_kprocessors[i]->cnt_suspend;
     t_idle          += kaapi_all_kprocessors[i]->t_idle;
+
+
+#if defined(KAAPI_USE_PERFCOUNTER)
+  /* */
+  if (default_param.display_perfcounter)
+  {
+    printf("----- Performance counters, core   : %i\n", i);
+    printf("%i: Total number of tasks executed     : %u\n", i, kaapi_all_kprocessors[i]->cnt_tasks);
+    printf("%i: Total number of steal OK requests  : %u\n", i, kaapi_all_kprocessors[i]->cnt_stealreqok);
+    printf("%i: Total number of steal BAD requests : %u\n", i, kaapi_all_kprocessors[i]->cnt_stealreq - kaapi_all_kprocessors[i]->cnt_stealreqok);
+    printf("%i: Total number of steal operations   : %u\n", i, kaapi_all_kprocessors[i]->cnt_stealop);
+    printf("%i: Total number of suspend operations : %u\n", i, kaapi_all_kprocessors[i]->cnt_suspend);
+    printf("%i: Total idle time                    : %e\n", i, kaapi_all_kprocessors[i]->t_idle);
+  }
+#endif  
 
     free(kaapi_all_kprocessors[i]);
     kaapi_all_kprocessors[i]= 0;
@@ -178,10 +205,6 @@ void __attribute__ ((destructor)) kaapi_fini(void)
   kaapi_all_kprocessors =0;
 
 #if defined(KAAPI_USE_PERFCOUNTER)
-  /* */
-  if (default_param.display_perfcounter)
-  {
-
 #ifndef PRIu64
 # if (sizeof(long) == sizeof(uint64_t))
 #  define PRIu64 "lu"
@@ -190,13 +213,17 @@ void __attribute__ ((destructor)) kaapi_fini(void)
 # endif
 #endif
 
-    printf("----- Performances counter\n");
-    printf("Total number of tasks executed    : %" PRIu64 "\n", cnt_tasks);
-    printf("Total number of steal OK requests : %" PRIu64 "\n", cnt_stealreqok);
-    printf("Total number of steal BAD requests: %" PRIu64 "\n", cnt_stealreq-cnt_stealreqok);
-    printf("Total number of steal operations  : %" PRIu64 "\n", cnt_stealop);
-    printf("Total idle time                   : %e\n", t_idle);
-    printf("Average steal request aggregation : %e\n", ((double)cnt_stealreq)/(double)cnt_stealop);
+  /* */
+  if (default_param.display_perfcounter)
+  {
+    printf("----- Cumulated Performance counters\n");
+    printf("Total number of tasks executed     : %" PRIu64 "\n", cnt_tasks);
+    printf("Total number of steal OK requests  : %" PRIu64 "\n", cnt_stealreqok);
+    printf("Total number of steal BAD requests : %" PRIu64 "\n", cnt_stealreq-cnt_stealreqok);
+    printf("Total number of steal operations   : %" PRIu64 "\n", cnt_stealop);
+    printf("Total number of suspend operations : %u\n", cnt_suspend);
+    printf("Total idle time                    : %e\n", t_idle);
+    printf("Average steal requests aggregation : %e\n", ((double)cnt_stealreq)/(double)cnt_stealop);
   }
 #endif  
   
