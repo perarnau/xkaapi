94af2d2ff678f8f87b1b56ad38001e9f3e96009c
Fabien Lementec <lementec@idkoiff.imag.fr>
2010-02-15 18:37:17 +0100

    [update] papi perf counters

--- src/machine/mt/kaapi_mt_perf.c
+++ src/machine/mt/kaapi_mt_perf.c
@@ -3,12 +3,67 @@
 
 
 
+/* internal */
+
+static int papi_event_codes[KAAPI_PERF_PAPI_MAX];
+static unsigned int papi_event_count = 0;
+
+static int get_papi_events(void)
+{
+  unsigned int i = 0;
+  unsigned int j;
+  const char* p;
+  const char* s;
+  char name[PAPI_MIN_STR_LEN];
+
+  s = getenv("KAAPI_PAPI_EVENTS");
+  if (s == NULL)
+    return 0;
+
+  while (*s)
+  {
+    if (i >= KAAPI_PERF_PAPI_MAX)
+      return -1;
+
+    p = s;
+
+    for (j = 0; j < (sizeof(name) - 1) && *s && (*s != ','); ++s, ++j)
+      name[j] = *s;
+    name[j] = 0;
+
+    if (PAPI_event_name_to_code(name, &papi_event_codes[i]) != PAPI_OK)
+      return -1;
+
+    ++i;
+
+    if (*s == 0)
+      break;
+
+    ++s;
+  }
+
+  papi_event_count = i;
+
+  return 0;
+}
+
+
+/* exported */
+
 void kaapi_perf_global_init(void)
 {
   /* must be called once */
 
-  kaapi_assert(PAPI_library_init(PAPI_VER_CURRENT) == PAPI_VER_CURRENT);
-  kaapi_assert(PAPI_thread_init(pthread_self) == PAPI_OK);
+  int error;
+
+  error = PAPI_library_init(PAPI_VER_CURRENT);
+  kaapi_assert_m(error, PAPI_VER_CURRENT, "PAPI_library_init()");
+  
+  error = PAPI_thread_init(pthread_self);
+  kaapi_assert_m(error, PAPI_OK, "PAPI_thread_init()");
+
+  error = get_papi_events();
+  kaapi_assert_m(0, error, "get_papi_events()");
 }
 
 
@@ -19,37 +74,23 @@ void kaapi_perf_global_fini(void)
 typedef kaapi_uint64_t kaapi_perf_counter_t;
 
 
-static void init_event_mapping(void)
-{
-  /* this is called in global_init
-     to build a backend context ie.
-     a papi event set for instance.
-     the list contains per backend
-     symbolics.
-   */
-
-  const char* const var = getenv("KAAPI_PERF_NAMES");
-  if (var == NULL)
-    return ;
-}
-
-
 void kaapi_perf_thread_init(void)
 {
-  /* retrieve the event mapping
-     of the following form
-     KAAPI_PERF_EVENTS=insn,l3
-   */
-
-  int papi_events[PAPI_NUM_EVENTS] = {PAPI_CODE_EVENTS};
-  kaapi_assert(PAPI_start_counters(papi_events, PAPI_NUM_EVENTS) == PAPI_OK);
+  if (papi_event_count)
+  {
+    const int err = PAPI_start_counters(papi_event_codes, papi_event_count);
+    kaapi_assert_m(PAPI_OK, err, "PAPI_start_counters()");
+  }
 }
 
 
 void kaapi_perf_thread_fini(void)
 {
-  long_long papi_values[PAPI_NUM_EVENTS];
-  kaapi_assert(PAPI_stop_counters(papi_values, PAPI_NUM_EVENTS) == PAPI_OK);
+  if (papi_event_count)
+  {
+    const int err = PAPI_stop_counters(NULL, papi_event_count);
+    kaapi_assert_m(err, PAPI_OK, "PAPI_stop_counters()");
+  }
 }
 
 
@@ -79,18 +120,28 @@ void kaapi_perf_accum_counters(void)
 {
   kaapi_processor_t* const kproc = kaapi_get_current_processor();
 
-  const int err = PAPI_accum_counters(kproc->papi_event_value, PAPI_NUM_EVENTS);
-  if (err != PAPI_OK)
-    printf("papi_error: %s(%s)\n", PAPI_strerror(err), __FUNCTION__);
+#if 0
+  if (papi_event_count)
+  {
+    const int err = PAPI_accum_counters(kproc->papi_event_value, papi_event_count);
+#if 0
+    if (err != PAPI_OK)
+      printf("papi_error: %s(%s)\n", PAPI_strerror(err), __FUNCTION__);
+#else
+    kaapi_assert_m(PAPI_OK, err, "PAPI_accum_counters");
+#endif
+  }
+#endif
 }
 
 
 void kaapi_perf_reduce_all_counters(kaapi_uint64_t* counters)
 {
+#if 0
   unsigned int j;
   unsigned int i;
 
-  for (j = 0; j < PAPI_NUM_EVENTS; ++j)
+  for (j = 0; j < papi_event_count; ++j)
     counters[j] = 0;
 
   /* snaps */
@@ -109,11 +160,13 @@ void kaapi_perf_reduce_all_counters(kaapi_uint64_t* counters)
     counters[j + 1] += kproc->cnt_stealreq;
     counters[j + 2] += kproc->cnt_stealop;
   }
+#endif
 }
 
 
 void kaapi_perf_reset_all_counters(void)
 {
+#if 0
   unsigned int j;
   unsigned int i;
 
@@ -128,4 +181,5 @@ void kaapi_perf_reset_all_counters(void)
     kproc->cnt_stealreq = 0;
     kproc->cnt_stealop = 0;
   }
+#endif
 }
