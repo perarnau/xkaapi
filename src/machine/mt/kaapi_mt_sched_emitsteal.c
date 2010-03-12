4eb8c8e135192deb405b474828e969fa70546181
Thierry Gautier <Thierry.Gautier@imag.fr>
2010-03-12 15:39:44 +0100

    Update

--- src/machine/mt/kaapi_mt_sched_emitsteal.c
+++ src/machine/mt/kaapi_mt_sched_emitsteal.c
@@ -98,6 +98,7 @@ redo_select:
   }
   kaapi_assert_debug( ok );
   kaapi_readmem_barrier();
+  kaapi_assert_debug( KAAPI_ATOMIC_READ(&victim.kproc->lock) == 1+kproc->kid );
   
   count = KAAPI_ATOMIC_READ( &victim.kproc->hlrequests.count );
   kaapi_assert_debug( count < KAAPI_MAX_PROCESSOR );
