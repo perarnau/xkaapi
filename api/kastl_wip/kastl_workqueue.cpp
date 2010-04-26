c6225f1f14aac65002a98fe02304aa78e2a6027c
le mentec fabien <fabien.lementec@imag.fr>
2010-04-26 12:05:10 +0200

    [update] merge kastl_workqueue

--- api/kastl_wip/kastl_workqueue.cpp
+++ api/kastl_wip/kastl_workqueue.cpp
@@ -52,7 +52,7 @@ namespace rts {
 /**
 */
 template<>
-void work_queue<64>::lock_pop()
+void work_queue_t<64>::lock_pop()
 {
   while (!_lock.cas( 0, 1))
     ;
@@ -62,7 +62,7 @@ void work_queue<64>::lock_pop()
 /**
 */
 template<>
-void work_queue<64>::lock_steal()
+void work_queue_t<64>::lock_steal()
 {
   while ((_lock.read() == 1) && !_lock.cas(0, 1))
     usleep(1);
@@ -72,15 +72,15 @@ void work_queue<64>::lock_steal()
 /**
 */
 template<>
-void work_queue<64>::unlock()
+void work_queue_t<64>::unlock()
 {
-  kaapi_mem_barrier();
+  kaapi_writemem_barrier();
   _lock.write(0);
 }
   
 /** */
 template<>
-bool work_queue<64>::slow_pop(range<64>& r, range<64>::size_type size)
+bool work_queue_t<64>::slow_pop(range_t<64>& r, size_type size)
 {
   /* already done in inlined pop :
       _beg += size;
@@ -118,7 +118,7 @@ bool work_queue<64>::slow_pop(range<64>& r, range<64>::size_type size)
 /**
 */
 template<>
-bool work_queue<64>::steal(range<64>& r, range<64>::size_type size)
+bool work_queue_t<64>::steal(range_t<64>& r, size_type size)
 {
   lock_steal();
   _end -= size;
@@ -126,10 +126,14 @@ bool work_queue<64>::steal(range<64>& r, range<64>::size_type size)
   if (_end < _beg)
   {
     _end += size;
+//printf("Fail to steal, queue is [%li,%li)\n", _beg, _end);
+//fflush(stdout);
     unlock();
     return false;
   }
   r.first = _end;  
+printf("%li::Ok to steal, queue is [%li,%li)\n", kaapi_get_elapsedns(), _beg, _end);
+fflush(stdout);
   unlock();
   r.last  = r.first + size;
   return true;
@@ -139,7 +143,7 @@ bool work_queue<64>::steal(range<64>& r, range<64>::size_type size)
 /**
 */
 template<>
-bool work_queue<64>::steal(range<64>& r, work_queue<64>::size_type size_max, work_queue<64>::size_type size_min)
+bool work_queue_t<64>::steal(range_t<64>& r, size_type size_max, size_type size_min)
 {
   kaapi_assert_debug( size_min <= size_max );
   lock_steal();
@@ -167,6 +171,60 @@ bool work_queue<64>::steal(range<64>& r, work_queue<64>::size_type size_max, wor
   
   return true;
 }  
+
+
+/**
+*/
+template<>
+bool work_queue_t<64>::steal_unsafe(range_t<64>& r, size_type size)
+{
+  _end -= size;
+  kaapi_mem_barrier();
+  if (_end < _beg)
+  {
+    _end += size;
+//printf("Fail to steal, queue is [%li,%li)\n", _beg, _end);
+//fflush(stdout);
+    return false;
+  }
+  r.first = _end;  
+  r.last  = r.first + size;
+//printf("%li::Ok steal [%li,%li), queue is [%li,%li)\n", kaapi_get_elapsedns(), r.first, r.last, _beg, _end);
+//fflush(stdout);
+  return true;
+}  
+
+
+/**
+*/
+template<>
+bool work_queue_t<64>::steal_unsafe(range_t<64>& r, size_type size_max, size_type size_min)
+{
+  kaapi_assert_debug( size_min <= size_max );
+  _end -= size_max;
+  kaapi_mem_barrier();
+  if (_end < _beg)
+  {
+    _end += size_max - size_min;
+    kaapi_mem_barrier();
+    if (_beg < _end)
+    {
+      r.first = _end;
+      r.last  = r.first+size_min;
+      return true;
+    }
+    _end += size_min; 
+    return false;
+  }
+  
+  r.first = _end;
+  r.last  = r.first + size_max;
+  
+  return true;
+}  
+
+
+
 
 } /* impl namespace */
 
