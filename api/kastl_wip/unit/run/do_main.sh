4f496b109a183a9b8cba9f1f378f6d54c1fa985f
Le Mentec Fabien <fabien.lementec@imag.fr>
2010-04-30 10:34:52 -0500

    [update] kastl/unit/do_main.sh wip

--- api/kastl_wip/unit/run/do_main.sh
+++ api/kastl_wip/unit/run/do_main.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
 
-
-ALGOS="inner_product"
-LIBS="stl kastl pastl tbb"
-DOS="bench check"
-
+# globals
+export LD_LIBRARY_PATH=$HOME/install/lib
 DIR=/tmp/out
-#SIZE=1000000
-#SIZE=1000000
 SIZE=1000000
 ITER=10
 #CPUSET=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
@@ -21,33 +16,83 @@ CPUSET=0,1
 #CPUSET=1
 
 
-rm -rf $DIR
-mkdir $DIR
+is_in_list() {
+    # $1 the list
+    # $2 the item
 
-#KAAPI_GLOBAL_OPTS=''
-#KAAPI_GLOBAL_OPTS='KAAPI_WSSELECT=workload KAAPI_DISPLAY_PERF=1'
-#KAAPI_GLOBAL_OPTS='KAAPI_WSSELECT=workload'
-KAAPI_GLOBAL_OPTS=''
-#KAAPI_GLOBAL_OPTS='KAAPI_DISPLAY_PERF=1'
-#KAAPI_GLOBAL_OPTS='KAAPI_WSSELECT=workload'
-#OUTPUT_FILTER=^.
-OUTPUT_FILTER='-v KAAPI'
-#OUTPUT_FILTER=^Total.idle
-#OUTPUT_FILTER=Total.idle
-#OUTPUT_FILTER='\[x\]'
-#OUTPUT_FILTER=^work:
+    for i in `echo $1 | cut -d\  -f1-`; do
+	if [ $i == $2 ] ; then
+	    return 1;
+	fi
+    done
 
-export LD_LIBRARY_PATH=$HOME/install/lib
+    return 0;
+}
 
-for ALGO in $ALGOS; do
-    for DO in $DOS; do
-	for LIB in $LIBS; do
-	    NAME=$ALGO-$LIB-$DO
-	    if [ -e ../bin/$NAME ]; then
-		OUTPUT_FILE=/tmp/oo/$NAME.o ;
-		KAAPI_CPUSET=$CPUSET ../bin/$NAME $SIZE $ITER > $OUTPUT_FILE ;
-		echo == ;
-	    fi
+
+gen_lists() {
+    ALGOS=''
+    LIBS=''
+    DOS=''
+
+    for f in $1/*; do
+	[ -e $f ] || continue ;
+
+	s=`basename $f`
+	s=(${s//-/ })
+	a=${s[0]}
+	l=${s[1]}
+	d=${s[2]}
+
+	is_in_list "$ALGOS" $a
+	if [ $? -eq 0 ] ; then
+	    ALGOS="$ALGOS $a" ;
+	fi
+
+	is_in_list "$LIBS" $l
+	if [ $? -eq 0 ] ; then
+	    LIBS="$LIBS $l" ;
+	fi
+
+	is_in_list "$DOS" $d
+	if [ $? -eq 0 ] ; then
+	    DOS="$DOS $d" ;
+	fi
+    done
+}
+
+
+print_lists() {
+    echo $ALGOS ;
+    echo $LIBS ;
+    echo $DOS ;
+}
+
+
+run_lists() {
+    rm -rf $DIR
+    mkdir $DIR
+
+    for ALGO in $ALGOS; do
+	for DO in $DOS; do
+	    for LIB in $LIBS; do
+		NAME=$ALGO-$LIB-$DO
+		if [ -e ../bin/$NAME ]; then
+		    OUTPUT_FILE=/tmp/oo/$NAME.o ;
+		    KAAPI_CPUSET=$CPUSET ../bin/$NAME $SIZE $ITER > $OUTPUT_FILE ;
+		    echo == ;
+		fi
+	    done
 	done
     done
-done
+}
+
+
+main() {
+    [ -d $1 ] || exit -1 ;
+
+    gen_lists $1;
+    run_lists ;
+}
+
+main $@;
