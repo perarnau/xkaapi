21100543e1d40e8f0530e75c37ab61f397f4a5b7
Le Mentec Fabien <fabien.lementec@imag.fr>
2010-05-03 04:20:46 -0500

    [update] do_main.sh, doc

--- api/kastl_wip/unit/run/do_main.sh
+++ api/kastl_wip/unit/run/do_main.sh
@@ -2,21 +2,52 @@
 
 # globals
 export LD_LIBRARY_PATH=$HOME/install/lib
-DIR=/tmp/out
-SIZE=100000
+SESSION_DIR=
+
+# input sizes
+SIZE0=1000
+SIZE1=10000
+SIZE2=20000
+#SIZE3=50000
+#SIZE4=100000
+#SIZE5=200000
+#SIZE6=1000000
+#SIZE7=
+#SIZE8=
+
+# iteration
 ITER=10
-#CPUSET=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-#CPUSET=8,9,10,11,12,13,14,15
-#CPUSET=0,1,2,3,4,5,6,7
-#CPUSET=5,7,9,11
-#CPUSET=7,9,11
-#CPUSET=9,11
-#CPUSET=9
-CPUSET=0,1
-#CPUSET=1
-ALGOS=min_element
-LIBS=kastl
-DOS=check
+
+# cpuset
+CPUSET0=1
+CPUSET1=0,1
+#CPUSET0=9
+#CPUSET1=9,11
+#CPUSET2=7,9,11
+#CPUSET3=5,7,9,11
+#CPUSET4=0,1,2,3,4,5,6,7
+#CPUSET5=8,9,10,11,12,13,14,15
+#CPUSET6=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+#CPUSET7=
+#CPUSET8=
+
+# {algo, lib, do}
+ALGOS='min_element'
+LIBS='stl kastl tbb'
+DOS='bench'
+
+
+gen_session_dir() {
+    if [ $SESSION_DIR'' == '' ] ; then
+	local h=`date +%S%j%Y` ;
+	export SESSION_DIR=../session/$h ;
+    fi
+
+    [ -d $SESSION_DIR ] || mkdir $SESSION_DIR
+
+    [ -h ../session/this ] && unlink ../session/this
+    ln -s $SESSION_DIR ../session/this
+}
 
 
 is_in_list() {
@@ -34,13 +65,13 @@ is_in_list() {
 
 
 gen_lists() {
-    DO_ALGOS=0 ;
-    DO_LIBS=0 ;
-    DO_DOS=0 ;
+    local DO_ALGOS=0 ;
+    local DO_LIBS=0 ;
+    local DO_DOS=0 ;
 
-    [ $ALGOS'' == '' ] && DO_ALGOS=1 ;
-    [ $LIBS'' == '' ] && DO_LIBS=1 ;
-    [ $DOS'' == '' ] && DO_DOS=1 ;
+    [ "$ALGOS"'' == '' ] && DO_ALGOS=1 ;
+    [ "$LIBS"'' == '' ] && DO_LIBS=1 ;
+    [ "$DOS"'' == '' ] && DO_DOS=1 ;
 
     for f in $1/*; do
 	[ -e $f ] || continue ;
@@ -49,7 +80,7 @@ gen_lists() {
 	s=(${s//-/ })
 
 	if [ $DO_ALGOS -eq 1 ]; then
-	    a=${s[0]}
+	    local a=${s[0]}
 	    is_in_list "$ALGOS" $a ;
 	    if [ $? -eq 0 ] ; then
 		ALGOS="$ALGOS $a" ;
@@ -57,7 +88,7 @@ gen_lists() {
 	fi
 
 	if [ $DO_LIBS -eq 1 ]; then
-	    l=${s[1]}
+	    local l=${s[1]}
 	    is_in_list "$LIBS" $l ;
 	    if [ $? -eq 0 ] ; then
 		LIBS="$LIBS $l" ;
@@ -65,7 +96,7 @@ gen_lists() {
 	fi
 
 	if [ $DO_DOS -eq 1 ]; then
-	    d=${s[2]}
+	    local d=${s[2]}
 	    is_in_list "$DOS" $d ;
 	    if [ $? -eq 0 ] ; then
 		DOS="$DOS $d" ;
@@ -83,18 +114,15 @@ print_lists() {
 
 
 run_lists() {
-    rm -rf $DIR
-    mkdir $DIR
-
-    print_lists ;
-
+    # global variables that need to be defined
+    # SIZE, CPUSET, CPUSETID
     for ALGO in $ALGOS; do
 	for DO in $DOS; do
 	    for LIB in $LIBS; do
 		NAME=$ALGO-$LIB-$DO
 		if [ -e ../bin/$NAME ]; then
-		    OUTPUT_FILE=/tmp/oo/$NAME.o ;
-		    KAAPI_CPUSET=$CPUSET ../bin/$NAME $SIZE $ITER > $OUTPUT_FILE ;
+		    OUTPUT_FILE=$SESSION_DIR/$NAME-$SIZE-$CPUSETID ;
+		    KAAPI_CPUSET=$CPUSET ../bin/$NAME $SIZE $ITER | grep -v KAAPI > $OUTPUT_FILE ;
 		    echo == ;
 		fi
 	    done
@@ -103,11 +131,54 @@ run_lists() {
 }
 
 
-main() {
-    [ -d $1 ] || exit -1 ;
-
-    gen_lists $1;
+run_cpuset() {
+    [ -z "$1" ] && return ;
+    CPUSET=$1 ;
+    CPUSETID=$2 ;
     run_lists ;
 }
 
-main $@;
+
+run_cpusets() {
+    run_cpuset "$CPUSET0" '0' ;
+    run_cpuset "$CPUSET1" '1' ;
+    run_cpuset "$CPUSET2" '2' ;
+    run_cpuset "$CPUSET3" '3' ;
+    run_cpuset "$CPUSET4" '4' ;
+    run_cpuset "$CPUSET5" '5' ;
+    run_cpuset "$CPUSET6" '6' ;
+    run_cpuset "$CPUSET7" '7' ;
+    run_cpuset "$CPUSET8" '8' ;
+}
+
+
+run_size() {
+    [ -z "$1" ] && return ;
+    SIZE=$1 ;
+    run_cpusets ;
+}
+
+
+run_sizes() {
+    run_size "$SIZE0" '0' ;
+    run_size "$SIZE1" '1' ;
+    run_size "$SIZE2" '2' ;
+    run_size "$SIZE3" '3' ;
+    run_size "$SIZE4" '4' ;
+    run_size "$SIZE5" '5' ;
+    run_size "$SIZE6" '6' ;
+    run_size "$SIZE7" '7' ;
+    run_size "$SIZE8" '8' ;
+}
+
+
+
+main() {
+    [ -d $1 ] || exit -1 ;
+
+    gen_session_dir ;
+    gen_lists $1 ;
+    run_sizes ;
+}
+
+main $@ ;
