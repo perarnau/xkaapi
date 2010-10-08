545516511e1ae4a0c88dbd58965f93fe6c2f45b9
Thierry Gautier <gautier@idkoiff.imag.fr>
2010-10-08 23:03:17 +0200

    Update opt fibo

--- examples/fibo/fibo_kaapixx_opt.cpp
+++ examples/fibo/fibo_kaapixx_opt.cpp
@@ -115,9 +115,9 @@ struct TaskBodyCPU<TaskFibo> : public TaskFibo {
       /* the Spawn keyword is used to spawn new task
        * new task is executed in parallel as long as dependencies are respected
        */
-      thread->Spawn<TaskFibo>() ( res1, n-1);
+      thread->Spawn<TaskFibo>() ( res1, n-2);
       //thread->Spawn<TaskFibo>() ( res2, n-2);
-      (*this) ( thread, res2, n-2);
+      (*this) ( thread, res2, n-1);
 
       /* the Sum task depends on res1 and res2 which are written by previous tasks
        * it must wait until thoses tasks are finished
