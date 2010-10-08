ea1e3938249c358c91097a0c937c6a495a42488c
Thierry Gautier <Thierry.Gautier@imag.fr>
2010-10-08 08:53:24 +0200

    Suppress and correct unsed file, comments,...

--- src/static/kaapi_threadgroup_checkdeps.c
+++ src/static/kaapi_threadgroup_checkdeps.c
@@ -7,7 +7,6 @@
  **
  ** Contributors :
  ** thierry.gautier@imag.fr
- ** theo.trouillon@imag.fr
  ** 
  ** This software is a computer program whose purpose is to execute
  ** multithreaded computation with data flow synchronization between
