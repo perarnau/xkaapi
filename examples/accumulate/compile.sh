57831891b289538156bf2e0d239842e522561cab
Thierry Gautier <Thierry.Gautier@imag.fr>
2010-10-11 21:52:35 +0200

    Delete file to compil/run

--- examples/accumulate/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-gcc \
-    -std=c99 -Wall -O3 \
-    -I/home/lementec/install/xkaapi_release/include \
-    -L/home/lementec/install/xkaapi_release/lib \
-    -o accumulate \
-    accumulate.c \
-    -lxkaapi
