7ad6e7fbbd6622c2eed84c37c63c90a78ecdbdd2
le mentec fabien <fabien.lementec@imag.fr>
2011-01-11 08:52:30 +0100

    [add] transform++ gpu example

---
+++ examples/gpu/transform++/build++.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+XKAAPIDIR=$HOME/install/xkaapi_gpu
+CUDADIR=/usr/local/stow/cuda-3.2/cuda
+
+$CUDADIR/bin/nvcc -w \
+    -I$XKAAPIDIR/include \
+    -I$CUDADIR/include \
+    -DKAAPI_NDEBUG=1 \
+    -DKAAPI_DEBUG=0 \
+    -o transform \
+    transform++.cu \
+    -L$XKAAPIDIR/lib -lkaapi++ \
+    -L$CUDADIR/lib64 -lcuda
