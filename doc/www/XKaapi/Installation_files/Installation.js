10fc9d6188dab84c458c2935996f852a97fd043d
Thierry Gautier <Thierry.Gautier@imag.fr>
2011-02-20 21:53:54 +0100

    [add] new web site for XKaapi

---
+++ doc/www/XKaapi/Installation_files/Installation.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.3 local-build-20110220
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Installation_files/InstallationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
