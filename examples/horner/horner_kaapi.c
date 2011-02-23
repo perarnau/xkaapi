3073b3748f962acad9c880d63d3c1929b57fd7fe
le mentec fabien <fabien.lementec@imag.fr>
2011-02-23 11:16:15 +0100

    [update, add] add horner_kaapi++ example. exposes the future c++ api for linear work adaptive processing

--- examples/horner/horner_kaapi.c
+++ examples/horner/horner_kaapi.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include "modp.h"
 #include "kaapi.h"
 
 
@@ -13,61 +14,6 @@
    for parallelization. sequential codes are
    provided for testing purposes.
  */
-
-
-/* modp arithmetics
- */
-
-static inline unsigned long modp
-(unsigned long n)
-{ return n % 1001; }
-
-static inline unsigned long mul_modp
-(unsigned long a, unsigned long b)
-{
-  /* (a * b) mod p */
-  return modp(a * b);
-}
-
-static inline unsigned long add_modp
-(unsigned long a, unsigned long b)
-{
-  /* (a + b) mod p */
-  return modp(a + b);
-}
-
-static inline unsigned long pow_modp
-(unsigned long a, unsigned long n)
-{
-  /* (x^y) mod p */
-
-  unsigned long an;
-
-  if (n == 0) return 1;
-  else if (n == 1) return a;
-
-  /* odd */
-  if (n & 1)
-    return mul_modp(a, pow_modp(a, n - 1));
-  
-  /* even */
-  an = pow_modp(a, n / 2);
-  return mul_modp(an, an);
-}
-
-static inline unsigned long axnb_modp
-(unsigned long a, unsigned long x, unsigned long n, unsigned long b)
-{
-  /* (a * x^n + b) mod p */
-  return add_modp(mul_modp(a, pow_modp(x, n)), b);
-}
-
-static inline unsigned long axb_modp
-(unsigned long a, unsigned long x, unsigned long b)
-{
-  /* (a * x + b) mod p */
-  return add_modp(mul_modp(a, x), b);
-}
 
 
 /* degree to index translation routines
