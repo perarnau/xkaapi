a5097b7162357342ac3c74e0ba7343d7cd758570
Thierry Gautier <Thierry.Gautier@imag.fr>
2011-03-07 17:36:12 +0100

    [fix] add case where null task (case that occurs when suspend thread from tasklist execution)

--- src/workstealing/kaapi_sched_suspend.c
+++ src/workstealing/kaapi_sched_suspend.c
@@ -117,7 +117,7 @@ int kaapi_sched_suspend ( kaapi_processor_t* kproc )
 
     if (kproc->thread == thread_condition) 
     {
-      kaapi_assert(kproc->thread->sfp->pc == task_condition);
+      kaapi_assert((tasklist !=0) || (kproc->thread->sfp->pc == task_condition));
 #if defined(KAAPI_USE_PERFCOUNTER)
       kaapi_perf_thread_stopswapstart(kproc, KAAPI_PERF_USER_STATE );
 #endif
--- src/workstealing/kaapi_sched_wakeup.c
+++ src/workstealing/kaapi_sched_wakeup.c
@@ -47,10 +47,10 @@
 #include "kaapi_impl.h"
 
 kaapi_thread_context_t* kaapi_sched_wakeup ( 
-    kaapi_processor_t* kproc, 
-    kaapi_processor_id_t kproc_thiefid, 
+    kaapi_processor_t*      kproc, 
+    kaapi_processor_id_t    kproc_thiefid, 
     kaapi_thread_context_t* cond_thread,
-    kaapi_task_t* cond_task 
+    kaapi_task_t*           cond_task 
   )
 {
   kaapi_thread_context_t* thread;
@@ -69,7 +69,7 @@ kaapi_thread_context_t* kaapi_sched_wakeup (
   */ 
   if (cond_thread !=0)
   {
-    if ( ((cond_thread->sfp->pc ==cond_task) && kaapi_thread_isready(cond_thread)) )
+    if ( ( ((cond_task ==0) || (cond_thread->sfp->pc ==cond_task)) && kaapi_thread_isready(cond_thread)) )
 //      || ((cond_thread->tasklist !=0) && !kaapi_tasklist_isempty(cond_thread->tasklist) ) )
 //             ((cond_thread->tasklist->recvlist!=0) 
 //          || (KAAPI_ATOMIC_READ(&cond_thread->tasklist->count_recv) ==0))
