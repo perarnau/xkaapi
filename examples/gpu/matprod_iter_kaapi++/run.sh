0a77d6633da2ee6d2d80e6b17f3a505a9b33f882
le mentec fabien <fabien.lementec@imag.fr>
2011-03-08 17:00:03 +0100

    [fix] test for empty KAAPI_CPUSET string before parsing. allow to use only one gpu.

--- examples/gpu/matprod_iter_kaapi++/run.sh
+++ examples/gpu/matprod_iter_kaapi++/run.sh
@@ -10,7 +10,8 @@ export LD_LIBRARY_PATH=/home/lementec/install/xkaapi_gpu/lib:$LD_LIBRARY_PATH
 #export KAAPI_GPUSET='0~14,1~15'
 #export KAAPI_GPUSET='0~14'
 
-export KAAPI_CPUSET=0
-export KAAPI_GPUSET='1~14'
+# export KAAPI_CPUSET=0
+export KAAPI_CPUSET=
+export KAAPI_GPUSET='1~0'
 
 ./a.out 2
