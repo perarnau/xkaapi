5b581dd5cb95325a8d0f2a75a137bf9bf6653c28
le mentec fabien <fabien.lementec@imag.fr>
2011-03-09 15:21:27 +0100

    [wip, cuda] kaapi_cuda_thread_execframe_tasklist compiling, to be tested

--- src/machine/cuda/kaapi_cuda_thread_execframe_tasklist.c
+++ src/machine/cuda/kaapi_cuda_thread_execframe_tasklist.c
@@ -41,235 +41,442 @@
 ** terms.
 ** 
 */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include "kaapi_impl.h"
 #include "kaapi_tasklist.h"
+#include "kaapi_cuda_error.h"
 
-/** Use the readylist and list of task to execute tasks.
-    If the list doesnot have ready tasks but all the tasks
-    are note executed, then return EWOULDBLOCK.
-    At the begining of the algorithm, if the td_ready container is
-    not allocated, the function allocated it. It is a stack
-    ready tasks : each time a task becomes ready, it is pushed on the
-    top of the task. 
-    The stack can be steal : the workqueue ensure consistency between
-    push/pop and steal operations.
-    
-    If the execution of a task creates new tasks, then the function execute
-    then using the standard DFG execframe function.
-*/
-int kaapi_cuda_thread_execframe_tasklist( kaapi_thread_context_t* thread )
+
+/* wait fifo.
+   cuda stream notifications dont pass
+   data back to the user, so we need a
+   way to associate a stream event with
+   some app specific data.
+ */
+
+typedef struct wait_node
 {
-  kaapi_task_t*              pc;      /* cache */
-  kaapi_workqueue_index_t    local_beg, local_end;
-  kaapi_tasklist_t*          tasklist;
-  kaapi_taskdescr_t**        td_top;  /*cache of tasklist->td_top */
-  kaapi_taskdescr_t*         td;
-  kaapi_task_body_t          body;
-  kaapi_frame_t*             fp;
-  kaapi_activationlink_t*    curr;
-  unsigned int               proc_type;
-  int                        task_pushed = 0;
-  int                        err;
-  uint32_t                   cnt_exec = 0;
+  void* data;
+  struct wait_node* prev;
+  struct wait_node* next;
+} wait_node_t;
 
-  printf("%s\n", __FUNCTION__);
+typedef struct wait_fifo
+{
+  CUstream stream;
+  wait_node_t* head;
+  wait_node_t* tail;
+} wait_fifo_t;
 
-  kaapi_assert_debug( thread->sfp >= thread->stackframe );
-  kaapi_assert_debug( thread->sfp < thread->stackframe+KAAPI_MAX_RECCALL );
-  tasklist = thread->sfp->tasklist;
-  kaapi_assert_debug( tasklist != 0 );
-  
-  /* get the processor type to select correct entry point */
-  proc_type = thread->proc->proc_type;
-    
-  /* alloc to much tasks descr ? -> */
-  if (tasklist->td_ready == 0)
+static inline int wait_fifo_create(wait_fifo_t* fifo)
+{
+  const CUresult res = cuStreamCreate(&fifo->stream, 0);
+  if (res != CUDA_SUCCESS)
   {
-    tasklist->td_ready = 
-      (kaapi_taskdescr_t**)malloc( 
-            (size_t) (sizeof(kaapi_taskdescr_t*) * tasklist->cnt_tasks) 
-    );
-    kaapi_workqueue_index_t ntasks = 0;
-
-    tasklist->recv = tasklist->recvlist.front;
-
-    curr = tasklist->readylist.front;
-    /* WARNING: the task pushed into the ready list should be in the push in the front 
-       if we want to ensure at least creation order 
-    */
-    td_top = tasklist->td_ready + tasklist->cnt_tasks;
-    while (curr !=0)
-    {
-      *--td_top = curr->td;
-      ++ntasks;
-      curr = curr->next;
-    }
-    /* the initial workqueue is [-ntasks, 0) at the begining of td_top; */
-    kaapi_writemem_barrier();
-    tasklist->td_top = tasklist->td_ready + tasklist->cnt_tasks;
-    kaapi_writemem_barrier();
-    kaapi_workqueue_init(&tasklist->wq_ready, -ntasks, 0);
+    kaapi_cuda_error("cuStreamCreate", res);
+    return -1;
   }
 
-  /* here we assume that execframe was already called 
-     - only reset td_top
-  */
-  td_top = tasklist->td_top;
-  
-  /* jump to previous state if return from suspend 
-     (if previous return from EWOULDBLOCK)
-  */
-  switch (tasklist->context.chkpt) {
-    case 1:
-      td = tasklist->context.td;
-      fp = tasklist->context.fp;
-      goto redo_frameexecution;
-    default:
-      break;
-  };
+  fifo->head = NULL;
+  fifo->tail = NULL;
 
+  return 0;
+}
+
+static inline int wait_fifo_destroy(wait_fifo_t* fifo)
+{
+  wait_node_t* pos = fifo->head;
+
+  while (pos != NULL)
+  {
+    wait_node_t* const tmp = pos;
+    pos = pos->next;
+    free(tmp);
+  }
+
+  fifo->head = NULL;
+  fifo->tail = NULL;
+
+  cuStreamDestroy(fifo->stream);
+
+  return 0;
+}
+
+static int wait_fifo_push(wait_fifo_t* fifo, void* data)
+{
+  /* todo: fifo specific allocator */
+  wait_node_t* const node = malloc(sizeof(wait_node_t));
+  if (node == NULL) return -1;
+
+  if (fifo->tail == NULL)
+    fifo->tail = node;
+  else
+    fifo->head->prev = node;
+
+  node->prev = NULL;
+  node->next = fifo->head;
+  fifo->head = node;
+
+  node->data = data;
+
+  return 0;
+}
+
+static void* wait_fifo_pop(wait_fifo_t* fifo)
+{
+  /* assume fifo not empty */
+  wait_node_t* const node = fifo->tail;
+  void* const data = node->data;
+
+  fifo->tail = node->prev;
+
+  if (node->prev == NULL)
+    fifo->head = NULL;
+  else
+    fifo->tail->next = NULL;
+
+  free(node);
+
+  return data;
+}
+
+static inline unsigned int wait_fifo_is_empty(wait_fifo_t* fifo)
+{
+  return fifo->head == NULL;
+}
+
+static inline unsigned int wait_fifo_is_ready(wait_fifo_t* fifo)
+{
+  /* return 1 if the top node is ready */
+  const CUresult res = cuStreamQuery(fifo->stream);
+
+  /* query and mark as empty is signaled */
+  if (res == CUDA_SUCCESS) return 1;
+
+#if defined(KAAPI_DEBUG)
+  if (res != CUDA_ERROR_NOT_READY)
+  {
+    kaapi_cuda_error("cuStreamQuery", res);
+    exit(-1);
+  }
+#endif
+
+  return 0;
+}
+
+static inline void* wait_fifo_next(wait_fifo_t* fifo)
+{
+  /* return NULL if empty or not ready */
+  if (wait_fifo_is_empty(fifo)) return NULL;
+  else if (wait_fifo_is_ready(fifo) == 0) return NULL;
+  return wait_fifo_pop(fifo);
+}
+
+
+/* wait port.
+   a wait port consists of 3 wait fifos
+   for input, kernel and output streams.
+ */
+
+typedef struct wait_port
+{
+  wait_fifo_t input_fifo;
+  wait_fifo_t output_fifo;
+  wait_fifo_t kernel_fifo;
+} wait_port_t;
+
+static void wait_port_destroy(wait_port_t* wp)
+{
+  /* assume wait_port_create() returned 0 */
+  wait_fifo_destroy(&wp->input_fifo);
+  wait_fifo_destroy(&wp->output_fifo);
+  wait_fifo_destroy(&wp->kernel_fifo);
+}
+
+static int wait_port_create(wait_port_t* wp)
+{
+  if (wait_fifo_create(&wp->input_fifo))
+  {
+    return -1;
+  }
+  if (wait_fifo_create(&wp->output_fifo))
+  {
+    wait_fifo_destroy(&wp->input_fifo);
+    return -1;
+  }
+  else if (wait_fifo_create(&wp->kernel_fifo))
+  {
+    wait_fifo_destroy(&wp->input_fifo);
+    wait_fifo_destroy(&wp->output_fifo);
+    return -1;
+  }
+
+  return 0;
+}
+
+static int wait_port_next(wait_port_t* wp, void** data)
+{
+  /* return 0 if something ready, -1 otherwise */
+
+  if ((*data = wait_fifo_next(&wp->input_fifo))) return 0;
+  else if ((*data = wait_fifo_next(&wp->output_fifo))) return 0;
+  else if ((*data = wait_fifo_next(&wp->kernel_fifo))) return 0;
+  return -1;
+}
+
+static unsigned int wait_port_is_empty(wait_port_t* wp)
+{
+  /* return 1 if all fifos empty */
+
+  if (wait_fifo_is_empty(&wp->input_fifo))
+    if (wait_fifo_is_empty(&wp->kernel_fifo))
+      if (wait_fifo_is_empty(&wp->output_fifo))
+	return 1;
+
+  return 0;
+}
+
+
+/* refer to kaapi_thread_execframe_tasklist.c
+   for general comments.
+   todos:
+   . newly created tasks not implemented
+ */
+int kaapi_cuda_thread_execframe_tasklist
+(kaapi_thread_context_t* thread)
+{
+  /* thread->sfp->tasklist */
+  kaapi_tasklist_t* tl;
+
+  /* tl->td_top */
+  kaapi_taskdescr_t** td_top;
+
+  /* the current task descriptor */
+  kaapi_taskdescr_t* td;
+
+  /* the current activation link */
+  kaapi_activationlink_t* al;
+
+  /* the task to process */
+  kaapi_task_t* pc;
+
+  /* pc->body[proc_type] */
+  kaapi_task_body_t body;
+
+  /* tasks are in the ready queue */
+  unsigned int has_ready;
+
+  /* tasks have been pushed */
+  unsigned int has_pushed;
+
+  /* local workqueue bounds */
+  kaapi_workqueue_index_t local_beg;
+  kaapi_workqueue_index_t local_end;
+
+  /* current frame */
+  kaapi_frame_t* fp;
+
+  /* executed task counter */
+  uint32_t cnt_exec = 0;
+
+  /* temporary error */
+  int err;
+
+  /* proc->proc_type */
+  const unsigned int proc_type = thread->proc->proc_type;
+
+  /* completion port */
+  wait_port_t wp;
+
+  /* toremove */
+  printf("%s\n", __FUNCTION__);
+  /* toremove */
+
+  /* todo: should be done once per proc */
+  if (wait_port_create(&wp) == -1) return -1;
+
+  /* bootstrap the readylist */
+  kaapi_assert_debug(thread->sfp >= thread->stackframe);
+  kaapi_assert_debug(thread->sfp < thread->stackframe + KAAPI_MAX_RECCALL);
+  kaapi_assert_debug(thread->sfp->tasklist != 0);
+  tl = thread->sfp->tasklist;
+
+  if (tl->td_ready == 0)
+  {
+    kaapi_workqueue_index_t ntasks = 0;
+
+    tl->td_ready = malloc(sizeof(kaapi_taskdescr_t*) * tl->cnt_tasks);
+    kaapi_assert_debug(tl->td_ready != NULL);
+
+    tl->recv = tl->recvlist.front;
+    
+    td_top = tl->td_ready + tl->cnt_tasks;
+
+    for (al = tl->readylist.front; al != NULL; al = al->next)
+    {
+      *--td_top = al->td;
+      ++ntasks;
+    }
+
+    /* the initial workqueue is td_top[-ntasks, 0) */
+    kaapi_writemem_barrier();
+    tl->td_top = tl->td_ready + tl->cnt_tasks;
+    kaapi_writemem_barrier();
+    kaapi_workqueue_init(&tl->wq_ready, -ntasks, 0);
+  }
+
+  /* assume execframe was already called, only reset td_top */
+  td_top = tl->td_top;
+
+  /* jump to previous state if return from suspend (EWOULDBLOCK) */
+  if (tl->context.chkpt == 1)
+  {
+    td = tl->context.td;
+    fp = tl->context.fp;
+
+    err = kaapi_thread_execframe(thread);
+    if ((err == EWOULDBLOCK) || (err == EINTR)) 
+    {
+      tl->context.chkpt = 1;
+      tl->context.td = td;
+      tl->context.fp = fp;
+      tl->cnt_exectasks += cnt_exec;
+      return err;
+    }
+
+    goto do_activation;
+  }
+  
   /* push the frame for the next task to execute */
   fp = (kaapi_frame_t*)thread->sfp;
   thread->sfp[1].sp_data = fp->sp_data;
-  thread->sfp[1].pc      = fp->sp;
-  thread->sfp[1].sp      = fp->sp;
+  thread->sfp[1].pc = fp->sp;
+  thread->sfp[1].sp = fp->sp;
   
   /* force previous write before next write */
   kaapi_writemem_barrier();
 
   /* update the current frame */
   ++thread->sfp;
-  kaapi_assert_debug( thread->sfp - thread->stackframe <KAAPI_MAX_RECCALL);
+  kaapi_assert_debug(thread->sfp - thread->stackframe < KAAPI_MAX_RECCALL);
 
-  while (!kaapi_tasklist_isempty( tasklist ))
+  /* execute all the ready tasks */
+ do_ready:
+  while (kaapi_workqueue_isempty(&tl->wq_ready) == 0)
   {
-    err = kaapi_workqueue_pop(&tasklist->wq_ready, &local_beg, &local_end, 1);
-    if (err ==0)
+    err = kaapi_workqueue_pop(&tl->wq_ready, &local_beg, &local_end, 1);
+    if (err) continue ;
+
+    td = td_top[local_beg];
+
+    kaapi_assert_debug(td != NULL);
+    kaapi_assert_debug(td->task != NULL);
+    KAAPI_DEBUG_INST(td_top[local_beg] = NULL);
+
+    /* next ready task */
+    pc = td->task;
+
+    /* get the correct body for the proc type */
+    body = kaapi_task_getuserbody(pc);
+    if (td->fmt != NULL)
+      body = td->fmt->entrypoint_wh[proc_type];
+    kaapi_assert_debug(body);
+
+    body(pc->sp, (kaapi_thread_t*)thread->sfp);
+    ++cnt_exec;
+  } /* while_ready */
+
+  has_ready = 0;
+
+  /* receive incoming sync */
+/*  do_recv: */
+  if (tl->recv != NULL)
+  {
+    td_top[local_beg--] = tl->recv->td;
+    tl->recv = tl->recv->next;
+    kaapi_workqueue_push(&tl->wq_ready, 1 + local_beg);
+    has_ready = 1;
+  }
+
+  /* wait for any completion */
+/*  do_wait: */
+  while (wait_port_is_empty(&wp) == 0)
+  {
+    /* event pump, wait for next to complete */
+    while (wait_port_next(&wp, (void**)&td) == -1)
     {
-      task_pushed = 0;
-      td = td_top[local_beg];
-      kaapi_assert_debug( td != 0);
-      KAAPI_DEBUG_INST( td_top[local_beg] = 0 );
+      /* nothing completed, and ready available */
+      if (has_ready) goto do_ready;
+    }
 
-      pc = td->task;
-      if (pc !=0)
+  do_activation:
+    /* assume no task pushed */
+    has_pushed = 0;
+
+    /* does the completed task activate others */
+    if (!kaapi_activationlist_isempty(&td->list))
+    {
+      for (al = td->list.front; al != NULL; al = al->next)
       {
-        /* get the correct body for the proc type */
-        if (td->fmt ==0)
-        { /* currently some internal tasks do not have format */
-          body = kaapi_task_getuserbody( td->task );
-        }
-        else 
-        {
-          body = td->fmt->entrypoint_wh[proc_type];
-        }
-        kaapi_assert_debug(body != 0);
-
-	if ((uintptr_t)body == (uintptr_t)kaapi_taskmove_body)
+	if (kaapi_taskdescr_activated(al->td))
 	{
-	  /* todo: push in cuda stream */
-	  printf("taskmove: %lx\n", (uintptr_t)td);
+	  td_top[local_beg--] = al->td;
+	  has_pushed = 1;
 	}
-	else
-	{
-	  printf("taskop: %lx\n", (uintptr_t)td);
-	}
-
-        /* here... call the task*/
-        body( pc->sp, (kaapi_thread_t*)thread->sfp );
-        ++cnt_exec;
-        
-        /* new tasks created ? */
-        if (unlikely(fp->sp > thread->sfp->sp))
-        {
-redo_frameexecution:
-          err = kaapi_thread_execframe( thread );
-          if ((err == EWOULDBLOCK) || (err == EINTR)) 
-          {
-            tasklist->context.chkpt  = 1;
-            tasklist->context.td     = td;
-            tasklist->context.fp     = fp;
-            tasklist->cnt_exectasks += cnt_exec;
-            return err;
-          }
-          kaapi_assert_debug( err == 0 );
-        }
       }
+    }
 
-      /* push in the front the activated tasks */
-      if (!kaapi_activationlist_isempty(&td->list))
+    /* do bcast after child execution */
+    if (td->bcast != 0)
+    {
+      for (al = td->bcast->front; al != NULL; al = al->next)
       {
-        kaapi_activationlink_t* curr_activated = td->list.front;
-        while (curr_activated !=0)
-        {
-          if (kaapi_taskdescr_activated(curr_activated->td))
-	  {
-	    printf("activated(%lx)\n", (uintptr_t)curr_activated->td);
-            /* if non local -> push on remote queue ? */
-            td_top[local_beg--] = curr_activated->td;
-	  }
-	  else
-	  {
-	    printf("notYetActivated(%lx)\n", (uintptr_t)curr_activated->td);
-	  }
-          curr_activated = curr_activated->next;
-        }
-        task_pushed |= 1;
+	/* bcast task are always ready */
+	td_top[local_beg--] = al->td;
+	has_pushed = 1;
       }
-
-      /* do bcast after child execution (they can produce output data) */
-      if (td->bcast !=0) 
-      {
-        kaapi_activationlink_t* curr_activated = td->bcast->front;
-        while (curr_activated !=0)
-        {
-          /* bcast task are always ready */
-          td_top[local_beg--] = curr_activated->td;
-          curr_activated      = curr_activated->next;
-        }
-        task_pushed |= 1;
-      }      
-    }
-    
-    /* recv incomming synchronisation 
-       - process it before the activation list of the executed
-       in order to force directly activated task to be executed first.
-    */
-    if (tasklist->recv !=0)
-    {
-      td_top[local_beg--] = tasklist->recv->td;
-      tasklist->recv      = tasklist->recv->next;
-      task_pushed        |= 1;
     }
 
-    /* ok, now push pushed task into the wq */
-    if (task_pushed)
+    /* enqueue the pushed tasks */
+    if (has_pushed)
     {
-      kaapi_workqueue_push(&tasklist->wq_ready, 1+local_beg);
-      task_pushed = 0;
+      has_ready = 1;
+      kaapi_workqueue_push(&tl->wq_ready, 1 + local_beg);
     }
-  } /* while */
+
+  } /* while_wait_port */
+
+  /* execute tasks made ready */
+  if (has_ready) goto do_ready;
+
+ /* do_return: */
+
+  /* todo: move in kproc */
+  wait_port_destroy(&wp);
 
   /* pop frame */
   --thread->sfp;
 
   /* update executed tasks */
-  tasklist->cnt_exectasks += cnt_exec;
+  tl->cnt_exectasks += cnt_exec;
   
   /* signal the end of the step for the thread
      - if no more recv (and then no ready task activated)
   */
-  if (kaapi_tasklist_isempty(tasklist))
+  if (kaapi_tasklist_isempty(tl))
   {
-    tasklist->context.chkpt = 0;
+    tl->context.chkpt = 0;
 #if defined(KAAPI_DEBUG)
-    tasklist->context.td = 0;
-    tasklist->context.fp = 0;
+    tl->context.td = 0;
+    tl->context.fp = 0;
 #endif    
     return ECHILD;
   }
-  tasklist->context.chkpt = 2;
-  tasklist->context.td = 0;
-  tasklist->context.fp = 0;
+  tl->context.chkpt = 2;
+  tl->context.td = 0;
+  tl->context.fp = 0;
   return EWOULDBLOCK;
 }
