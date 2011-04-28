c8fef580d9c32688480e66df80b585fd7e42272d
le mentec fabien <fabien.lementec@imag.fr>
2011-04-28 13:35:24 +0200

    [rename] kaapi_thread_set_unstealable

--- src/thread/kaapi_thread_clear.c
+++ src/thread/kaapi_thread_clear.c
@@ -81,7 +81,7 @@ int kaapi_thread_clear( kaapi_thread_context_t* thread )
 
 
 
-void kaapi_set_unstealable(unsigned int fu)
+void kaapi_thread_set_unstealable(unsigned int fu)
 {
   kaapi_thread_context_t* const c = kaapi_self_thread_context();
   kaapi_mem_barrier();
