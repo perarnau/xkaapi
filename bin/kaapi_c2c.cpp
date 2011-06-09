7aa60aa7b1a1777c59b8749a45fe5d750439a1cb
Thierry Gautier <Thierry.Gautier@imag.fr>
2011-06-09 11:05:29 +0200

    Add license + todolist

--- bin/kaapi_c2c.cpp
+++ bin/kaapi_c2c.cpp
@@ -1,3 +1,49 @@
+/*
+** kaapi_staticsched.h
+** xkaapi
+** 
+** Created on Tue Mar 31 15:19:09 2009
+** Copyright 2009 INRIA.
+**
+** Contributors :
+**
+** thierry.gautier@inrialpes.fr
+** fabien.lementec@gmail.com / fabien.lementec@imag.fr
+** 
+** This software is a computer program whose purpose is to execute
+** multithreaded computation with data flow synchronization between
+** threads.
+** 
+** This software is governed by the CeCILL-C license under French law
+** and abiding by the rules of distribution of free software.  You can
+** use, modify and/ or redistribute the software under the terms of
+** the CeCILL-C license as circulated by CEA, CNRS and INRIA at the
+** following URL "http://www.cecill.info".
+** 
+** As a counterpart to the access to the source code and rights to
+** copy, modify and redistribute granted by the license, users are
+** provided only with a limited warranty and the software's author,
+** the holder of the economic rights, and the successive licensors
+** have only limited liability.
+** 
+** In this respect, the user's attention is drawn to the risks
+** associated with loading, using, modifying and/or developing or
+** reproducing the software by the user in light of its specific
+** status of free software, that may mean that it is complicated to
+** manipulate, and that also therefore means that it is reserved for
+** developers and experienced professionals having in-depth computer
+** knowledge. Users are therefore encouraged to load and test the
+** software's suitability as regards their requirements in conditions
+** enabling the security of their systems and/or data to be ensured
+** and, more generally, to use and operate it in the same conditions
+** as regards security.
+** 
+** The fact that you are presently reading this means that you have
+** had knowledge of the CeCILL-C license and that you accept its
+** terms.
+** 
+*/
+
 #include <rose.h>
 #include <string>
 #include <iostream>
@@ -8,15 +54,38 @@
 #include <ctype.h>
 #include <time.h>
 #include <stdexcept>
-/* TODO:
-OK   - gestion des args si paramètre range: begin -> access, end -> size_t (taille)
-   - reduction & parsing de la declaration des fonctions + support moteur executif
-     - init à 0 le 1er appel en CW ?
+/* TODO LIST
+  OK : fait et un peu tester
+  NOK: not OK
+  
+OK    - gestion des args si paramètre range: begin -> access, end -> size_t (taille)
+OK    - reduction & parsing de la declaration des fonctions + support moteur executif
+OK/NOK- les boucles sont gérés comme suit:
+        save_frame
+        for(... ) { ... }
+        restore_frame
+OK     Les break ne pose pas de problème. 
+OK     Les return non plus (l'appelant exécutant les tâches créées).
+NOK    Le cas des gotos n'est pas géré.
      
-   - vérification des accès effectués en fonction des modes déclarés
+NOK   - vérification des accès effectués en fonction des modes déclarés
+        - mode d'accès R/W/RW/CW
+          * arithmetique de pointeurs
+          * accès: R/RW/CW: rhs
+          * accès: W/CW/RW: lhs
+          * accès: CW pas de vérification supplémentaire sur l'ensemble des opérations 
+          séquentielle.
+        - portée des variables:
+          * scope de déclaration supérieur à l'appel de fonction
+            - ok pour les paramètres formel
+            - ok si sync ou fin de boucle (sync implicite) après l'utilisation par une tâche
+          * sinon émettre une erreur / warning ?
+
+NOK   - parsing de fin des pragmas: on ne vérifie pas la fin de la phrase...
 
    - adaptive loop. See OpenMP canonical form of Loop. Not all kind of expression
    can occurs in expressions of the for loop (init_expr; cond_expr; incr_expr )
+        - for_each: ok
 
    - generation of serialization operators
 */
