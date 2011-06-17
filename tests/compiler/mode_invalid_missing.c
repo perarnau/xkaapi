6f504ec1163cf64ef9c37d191b0125158a0dd065
le mentec fabien <fabien.lementec@imag.fr>
2011-06-17 09:53:31 -0500

    [add] compiler. mode_invalid_missing test

--- tests/compiler/mode_invalid_missing.c
+++ tests/compiler/mode_invalid_missing.c
@@ -1,21 +1,9 @@
-static void bar(unsigned int* m)
-{
-  if (*m > 3) bar(m);
-}
+extern void baz(unsigned int*);
 
 #pragma kaapi task write(n) read(m)
 static void fu(unsigned int* m, unsigned int* n)
 {
-  m += 2;
-  if (*m < 0) ;
-  *n = *m;
-  *n = 2 + *m;
-  m = 0;
-  n[(unsigned int)m + 5] = 0;
-  n[*m] = 0;
-  n[*m + 5] = 0;
-  *(unsigned int*)(*m + 2) = 0;
-  bar(m);
+  baz(m);
 }
 
 int main(int ac, char** av)
