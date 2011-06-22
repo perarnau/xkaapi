95959d6187f377f6f38702a7543d776e7d4a6bdc
le mentec fabien <fabien.lementec@imag.fr>
2011-06-22 09:50:08 +0200

    [update] configured block size

--- tests/compiler/hist.c
+++ tests/compiler/hist.c
@@ -3,10 +3,10 @@
 
 
 /* define the hist level too. assume <= 32. */
-#define CONFIG_DATA_LOG2 12
+#define CONFIG_DATA_LOG2 13
 /* dont touch */
 #define CONFIG_DATA_DIM (1 << CONFIG_DATA_LOG2)
-#define CONFIG_BLOCK_DIM (CONFIG_DATA_DIM / 4)
+#define CONFIG_BLOCK_DIM (CONFIG_DATA_DIM / 8)
 
 
 /* histogram reduction */
