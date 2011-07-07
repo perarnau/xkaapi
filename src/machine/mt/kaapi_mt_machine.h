b338baedb43c590dfea8413e2f041495caa6cd77
Thierry Gautier <Thierry.Gautier@imag.fr>
2011-07-07 16:18:27 +0200

    fix bug due to concurrency on suspend/resume

--- src/machine/mt/kaapi_mt_machine.h
+++ src/machine/mt/kaapi_mt_machine.h
@@ -980,10 +980,10 @@ static inline int kaapi_request_post( kaapi_processor_id_t thief_kid, kaapi_repl
 #if defined(KAAPI_USE_BITMAP_REQUEST)
   kaapi_assert_debug((thief_kid >=0) && (thief_kid < KAAPI_MAX_PROCESSOR_LIMIT));
   req = &victim->hlrequests.requests[thief_kid];
-  req->kid   = thief_kid;
-  req->reply = reply;
+  req->kid       = thief_kid;
+  req->reply     = reply;
   reply->preempt = 0;
-  reply->status = KAAPI_REQUEST_S_POSTED;
+  reply->status  = KAAPI_REQUEST_S_POSTED;
   kaapi_writemem_barrier();
   kaapi_bitmap_set( &victim->hlrequests.bitmap, thief_kid );
   return 0;
