73c6e8dc7211ef75ccb6eadf664e1360b246a6c5
le mentec fabien <fabien.lementec@imag.fr>
2011-08-01 09:20:55 +0200

    [update] compiler test

--- tests/compiler/accumulate_adapt.c
+++ tests/compiler/accumulate_adapt.c
@@ -24,24 +24,61 @@ static real_type accumulate(const real_type* v, unsigned int n)
   unsigned int i;
   real_type sum = 0;
 #pragma kaapi loop reduction(reduce_sum_id:sum)
-  for (i = 0; i < n; ++i) sum += v[i] * sin(v[i]) * cos(v[i]);
+  for (i = 0; i < n; ++i) sum += v[i];
   return sum;
 }
+
+
+/* temporary */
+
+#ifndef __USE_BSD
+# define __USE_BSD
+#endif
+
+#include <time.h>
+#include <sys/time.h>
+
+struct timeval tms[3];
+
+static void pragma_kaapi_start_time(void)
+{
+  gettimeofday(&tms[0], NULL);
+}
+
+static void pragma_kaapi_stop_time(void)
+{
+  double msecs;
+  gettimeofday(&tms[1], NULL);
+  timersub(&tms[1], &tms[0], &tms[2]);
+  msecs = (double)tms[2].tv_sec * 1E3 + (double)tms[2].tv_usec / 1E3;
+  printf("%lf\n", msecs);
+}
+
 
 /* main */
 
 int main(int ac, char** av)
 {
-  static const unsigned int n = 32 * 1024 * 1024;
+  static const unsigned int n = 4 * 1024 * 1024;
   real_type* const v = malloc(n * sizeof(real_type));
   real_type sum;
+  unsigned int j;
   unsigned int i;
   for (i = 0; i < n; ++i) v[i] = 1;
 
 #pragma kaapi parallel
-  sum = accumulate(v, n);
+  {
+    pragma_kaapi_start_time();
 
-  printf("%lf\n", sum);
+    for (j = 0; j < 100; ++j)
+    {
+      sum = accumulate(v, n);
+      if (sum != n) printf("invalid: %lf\n", sum);
+    }
+
+    pragma_kaapi_stop_time();
+  }
+
   free(v);
   return 0;
 }
