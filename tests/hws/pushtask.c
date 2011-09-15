1c92a2036472b52a950d63af84633fe38e4a16e8
le mentec fabien <fabien.lementec@imag.fr>
2011-09-15 10:41:08 +0200

    [update] pushtask hws example

--- tests/hws/pushtask.c
+++ tests/hws/pushtask.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
 #include "kaapi.h"
 
 
-static void numa_task(void* p)
+static void numa_task(void* p, kaapi_thread_t* t)
 {
-  printf("[%u] %s\n", kaapi_get_self_kid(), __FUNCTION__);  
+  printf("[%u] %s %lx\n", kaapi_get_self_kid(), __FUNCTION__, (uintptr_t)p);
 }
 
 
-static void flat_task(void* p)
+static void flat_task(void* p, kaapi_thread_t* t)
 {
-  printf("[%u] %s\n", kaapi_get_self_kid(), __FUNCTION__);
+  printf("[%u] %s %lx\n", kaapi_get_self_kid(), __FUNCTION__, (uintptr_t)p);
 }
 
 
@@ -17,8 +19,8 @@ int main(int ac, char** av)
 {
   kaapi_init(1, &ac, &av);
 
-  kaapi_hws_pushtask_flat(flat_task, NULL);
-  kaapi_hws_pushtask_numa(numa_task, NULL);
+  kaapi_hws_pushtask_flat(flat_task, (void*)(uintptr_t)42);
+  kaapi_hws_pushtask_numa(numa_task, (void*)(uintptr_t)24);
   kaapi_sched_sync();
   while (1) ;
 
