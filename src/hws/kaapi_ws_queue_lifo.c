40d679a687c8b0c2d93b0d580830ac387d2e809a
le mentec fabien <fabien.lementec@imag.fr>
2011-09-22 14:14:14 +0200

    [update] hws files licensing headers

--- src/hws/kaapi_ws_queue_lifo.c
+++ src/hws/kaapi_ws_queue_lifo.c
@@ -1,3 +1,50 @@
+/*
+** kaapi_ws_queue_lifo.c
+** xkaapi
+** 
+** Created on Tue Mar 31 15:19:14 2009
+** Copyright 2009 INRIA.
+**
+** Contributors :
+**
+** thierry.gautier@inrialpes.fr
+** fabien.lementec@gmail.com / fabien.lementec@imag.fr
+** 
+** This software is a computer program whose purpose is to execute
+** multithreaded computation with data flow synchronization between
+** threads.
+** 
+** This software is governed by the CeCILL-C license under French law
+** and abiding by the rules of distribution of free software.  You can
+** use, modify and/ or redistribute the software under the terms of
+** the CeCILL-C license as circulated by CEA, CNRS and INRIA at the
+** following URL "http://www.cecill.info".
+** 
+** As a counterpart to the access to the source code and rights to
+** copy, modify and redistribute granted by the license, users are
+** provided only with a limited warranty and the software's author,
+** the holder of the economic rights, and the successive licensors
+** have only limited liability.
+** 
+** In this respect, the user's attention is drawn to the risks
+** associated with loading, using, modifying and/or developing or
+** reproducing the software by the user in light of its specific
+** status of free software, that may mean that it is complicated to
+** manipulate, and that also therefore means that it is reserved for
+** developers and experienced professionals having in-depth computer
+** knowledge. Users are therefore encouraged to load and test the
+** software's suitability as regards their requirements in conditions
+** enabling the security of their systems and/or data to be ensured
+** and, more generally, to use and operate it in the same conditions
+** as regards security.
+** 
+** The fact that you are presently reading this means that you have
+** had knowledge of the CeCILL-C license and that you accept its
+** terms.
+** 
+*/
+
+
 #include <stdint.h>
 #include "kaapi_impl.h"
 
@@ -44,8 +91,6 @@ static kaapi_ws_error_t steal
   kaapi_request_t* req;
   unsigned int top;
   uintptr_t state;
-
-  /* printf("[%u] %s\n", kaapi_get_self_kid(), __FUNCTION__); */
 
   /* avoid to take the lock */
   if (q->top == 0) return KAAPI_WS_ERROR_EMPTY;
@@ -113,12 +158,7 @@ static kaapi_ws_error_t steal
 
       format = kaapi_format_resolvebybody(task_body);
 
-      if (format == NULL)
-      {
-	printf("-- format == null\n");
-	exit(-1);
-	continue ;
-      }
+      kaapi_assert_debug(format);
 
       state = kaapi_task_orstate(task, KAAPI_MASK_BODY_STEAL);
       if (kaapi_task_isstealable(task) == 0) continue ;
