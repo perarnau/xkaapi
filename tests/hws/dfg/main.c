c814a1fda2ccc7142a31b30ef79c60f36fd6f83b
le mentec fabien <fabien.lementec@imag.fr>
2011-10-04 15:21:07 +0200

    [update] tests/hws/dfg example

--- tests/hws/dfg/main.c
+++ tests/hws/dfg/main.c
@@ -24,11 +24,40 @@ static void flat_body(void* p, kaapi_thread_t* t)
 }
 
 
+static void numa_body(void* p, kaapi_thread_t* t)
+{
+  wrapped_uint_t* wui = (wrapped_uint_t*)p;
+
+  const unsigned int val = *kaapi_data(unsigned int, &wui->val);
+
+  printf("xx [%u] %s %u\n", kaapi_get_self_kid(), __FUNCTION__, val);
+  usleep(1000000);
+
+  *kaapi_data(unsigned int, &wui->val) = 3;
+}
+
+
 KAAPI_REGISTER_TASKFORMAT
 (
  flat_format,
  "flat",
  flat_body,
+ sizeof(wrapped_uint_t),
+ 1,
+ (kaapi_access_mode_t[]){ KAAPI_ACCESS_MODE_R },
+ (kaapi_offset_t[]){ offsetof(wrapped_uint_t, val.data) },
+ (kaapi_offset_t[]){ offsetof(wrapped_uint_t, val.version) },
+ (const struct kaapi_format_t*[]){ kaapi_int_format },
+ (struct kaapi_memory_view_t[]){},
+ 0
+)
+
+
+KAAPI_REGISTER_TASKFORMAT
+(
+ numa_format,
+ "numa",
+ numa_body,
  sizeof(wrapped_uint_t),
  1,
  (kaapi_access_mode_t[]){ KAAPI_ACCESS_MODE_R },
@@ -46,20 +75,30 @@ int main(int ac, char** av)
 
   kaapi_init(1, &ac, &av);
 
-  for (j = 0; j < 128; ++j)
+  for (j = 0; j < 4; ++j)
   {
     kaapi_thread_t* const thread = kaapi_self_thread();
     kaapi_task_t* const task = kaapi_thread_toptask(thread);
-    wrapped_uint_t* const flat_wui = kaapi_thread_pushdata_align
+
+    /* push for flat stealing */
+    wrapped_uint_t* wui = kaapi_thread_pushdata_align
       (thread, sizeof(wrapped_uint_t), sizeof(void*));
-
     kaapi_thread_allocateshareddata
-      (&flat_wui->val, thread, sizeof(unsigned int));
-    *kaapi_data(unsigned int, &flat_wui->val) = 42;
-
-    kaapi_task_initdfg(task, flat_body, flat_wui);
-
+      (&wui->val, thread, sizeof(unsigned int));
+    *kaapi_data(unsigned int, &wui->val) = 42;
+    kaapi_task_initdfg(task, flat_body, wui);
     kaapi_thread_pushtask(thread);
+
+#if 0
+    /* push for numa stealing */
+    /* todo: should be an allocator per ws_block, ie.
+       ws->allocate(). export hws_alloc on top of that.
+     */
+    wui = malloc(sizeof(wrapped_uint_t) + sizeof(unsigned int));
+    wui->val.data = (void*)((uintptr_t)wui + sizeof(wrapped_uint_t));
+    *kaapi_data(unsigned int, &wui->val) = 42;
+    kaapi_hws_pushtask_numa(numa_body, wui);
+#endif
   }
 
   kaapi_sched_sync();
