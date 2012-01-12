cbaade8d4f58da2624a62fca0e4d7fdacf938af3
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-01-12 16:57:46 +0100

    Syntax & indentation

--- src/workstealing/kaapi_sched_select_workload_rand.c
+++ src/workstealing/kaapi_sched_select_workload_rand.c
@@ -47,72 +47,71 @@
 
 
 /** Do rand selection 
-    adapted from kaapi_sched_select_victim_rand.c
-    steal a workloaded processor at random BUT try
-    zero first.
-*/
+ adapted from kaapi_sched_select_victim_rand.c
+ steal a workloaded processor at random BUT try
+ zero first.
+ */
 int kaapi_sched_select_victim_workload_rand
 (
-  kaapi_processor_t* kproc, 
-  kaapi_victim_t* victim, 
-  kaapi_selecvictim_flag_t flag 
+    kaapi_processor_t* kproc, 
+    kaapi_victim_t* victim, 
+    kaapi_selecvictim_flag_t flag 
 )
 {
   switch (flag)
   {
-  case KAAPI_SELECT_VICTIM:
+    case KAAPI_SELECT_VICTIM:
     {
       int nbproc, victimid;
       unsigned int ntries = 0;
       unsigned int do_zero = 1;
-
+      
       /* select a victim */
       if (kproc->fnc_selecarg[0] == 0) 
-	kproc->fnc_selecarg[0] = (uintptr_t)(long)rand();
-
+        kproc->fnc_selecarg[0] = (uintptr_t)(long)rand();
+      
     redo_select:
       nbproc = kaapi_count_kprocessors;
       if (nbproc <=1) return EINVAL;
-
+      
       if (do_zero == 1)
       {
-	victimid = 0;
-	do_zero = 0;
+        victimid = 0;
+        do_zero = 0;
       }
       else
       {
-	victimid = rand_r( (unsigned int*)&kproc->fnc_selecarg ) % nbproc;
+        victimid = rand_r( (unsigned int*)&kproc->fnc_selecarg ) % nbproc;
       }
-
+      
       /* Get the k-processor */    
       victim->kproc = kaapi_all_kprocessors[ victimid ];
       if (victim->kproc ==0) goto redo_select;
-
+      
       /* first workload barrier */
       if (ntries < (kaapi_count_kprocessors * 2))
       {
-	++ntries;
-
-	if (KAAPI_ATOMIC_READ(&victim->kproc->workload) == 0)
-	  goto redo_select;
+        ++ntries;
+        
+        if (KAAPI_ATOMIC_READ(&victim->kproc->workload) == 0)
+          goto redo_select;
       }
-
+      
       break ;
     }
-
-  default:
+      
+    default:
     {
       break ;
     }
   }
-
+  
   return 0;
 }
 
 
 /* workload accessors
  */
-
 void kaapi_set_workload(kaapi_processor_t* kproc, unsigned long workload)
 {
   KAAPI_ATOMIC_WRITE(&kproc->workload, workload);
