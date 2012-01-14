b340ea1d7b9b907df8cc5b6f6b44f1392cbbe7ff
François Broquedis <francois.broquedis@imag.fr>
2012-01-14 15:00:43 +0100

    Adapt the launcher scripts so they can be run on Mac OS X.

--- api/libgomp/tests/run-single-test.sh
+++ api/libgomp/tests/run-single-test.sh
@@ -4,5 +4,19 @@ if [ $# -ne 1 ];
 then
     echo "Usage: ./run-single-test.sh testname"
 fi
-  
-LD_PRELOAD=../.libs/libgomp.so ./$1
+
+KAAPI_INSTALL_PATH=/Users/broq/soft/install
+
+(uname -a | grep Darwin) > /dev/null 2> /dev/null
+DARWIN_SYS=$?
+
+if [ $DARWIN_SYS -eq 0 ]; 
+then
+    export DYLD_FORCE_FLAT_NAMESPACE=1
+    export DYLD_INSERT_LIBRARIES="$KAAPI_INSTALL_PATH/lib/libgomp.dylib"
+else
+    export LD_PRELOAD="../.libs/libgomp.so"
+fi
+
+# Run the test  
+./$1
--- api/libgomp/tests/run-tests.sh
+++ api/libgomp/tests/run-tests.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+KAAPI_INSTALL_PATH=/Users/broq/soft/install
+
 TESTS=hello
 
 make clean
@@ -9,9 +11,20 @@ if [ $# -ne 0 ];
 then
     TESTS=$@
 fi
+
+(uname -a | grep Darwin) > /dev/null 2> /dev/null
+DARWIN_SYS=$?
+
+if [ $DARWIN_SYS -eq 0 ]; 
+then
+    export DYLD_FORCE_FLAT_NAMESPACE=1
+    export DYLD_INSERT_LIBRARIES="$KAAPI_INSTALL_PATH/lib/libgomp.dylib"
+else
+    export LD_PRELOAD="../.libs/libgomp.so"
+fi
   
 for prog in $TESTS; 
 do
     echo "*** Running \"$prog\" test ***"
-    LD_PRELOAD=../.libs/libgomp.so ./$prog
+    ./$prog
 done
