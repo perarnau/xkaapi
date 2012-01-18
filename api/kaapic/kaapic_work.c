75a169e3836089bbcf589a7b4608cdc8ca09d94a
François Broquedis <francois.broquedis@imag.fr>
2012-01-18 11:21:47 +0100

    Merge from origin/master.

--- api/kaapic/kaapic_work.c
+++ api/kaapic/kaapic_work.c
@@ -677,7 +677,7 @@ continue_work:
 
   /* refill the workqueue from reseved task and continue */
   pos = work_array_first(w.wa);
-  kaapi_assert_debug( pos >0 );
+  kaapi_assert_debug( pos >=0 );
   work_array_pop(w.wa, pos, &i, &j);
 
   kaapi_workqueue_reset(
