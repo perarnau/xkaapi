4d1092d1e73104e82f5931ace88375f2861b41b3
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-01-30 14:52:06 +0100

    Add runtime functions support for libgomp over kaapi.

--- api/libgomp/tests/task-firstprivate.c
+++ api/libgomp/tests/task-firstprivate.c
@@ -8,31 +8,27 @@
 int
 main (int argc, char **argv)
 {
-  int res[8] = { 0 };
-#pragma omp parallel shared (res)
+ v#pragma omp parallel shared (res)
   {
 #pragma omp single 
     {
       int i;
       
       for (i = 0; i < 8; i++)
-	{
+      {
 #pragma omp task firstprivate (i) shared (res)
-	  {
-	    res[i]++;
-	  }
-	}
+        res[i]++;
+      }
     }
-
 #pragma omp taskwait
   }
-
+  
   int i, passed = OK;
   for (i = 0; i < 8; i++)
-    {
-      if (res[i] != 1)
-	passed = KO;
-    }
+  {
+    if (res[i] != 1)
+      passed = KO;
+  }
   
   test_check ("task-firstprivate", (passed == OK));
   
