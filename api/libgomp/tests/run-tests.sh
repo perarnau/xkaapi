72f83d80ea83a8af912fa02323169e6d9d42e595
Francois Broquedis <francois.broquedis@imag.fr>
2012-01-31 11:14:29 +0100

    libgomp: Fix the run-* scripts so they can be run on Linux platforms.

--- api/libgomp/tests/run-tests.sh
+++ api/libgomp/tests/run-tests.sh
@@ -29,7 +29,7 @@ then
     export DYLD_FORCE_FLAT_NAMESPACE=1
     export DYLD_INSERT_LIBRARIES="$KAAPI_INSTALL_PATH/lib/libkaapic.dylib:$KAAPI_INSTALL_PATH/lib/libgomp.dylib"
 else
-    export LD_PRELOAD="../.libs/libgomp.so"
+    export LD_PRELOAD="$KAAPI_INSTALL_PATH/lib/libkaapic.so:../.libs/libgomp.so"
 fi
   
 for prog in $TESTS; 
