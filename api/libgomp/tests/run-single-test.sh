6e72b1fbee4f5db6707dadad458d9fbb0d307088
Francois Broquedis <francois.broquedis@imag.fr>
2012-01-31 11:19:07 +0100

    libgomp: Get rid of relative paths when refering to our libgomp.so library.

--- api/libgomp/tests/run-single-test.sh
+++ api/libgomp/tests/run-single-test.sh
@@ -24,7 +24,7 @@ then
     export DYLD_FORCE_FLAT_NAMESPACE=1
     export DYLD_INSERT_LIBRARIES="$KAAPI_INSTALL_PATH/lib/libkaapic.dylib:$KAAPI_INSTALL_PATH/lib/libgomp.dylib"
 else
-    export LD_PRELOAD="$KAAPI_INSTALL_PATH/lib/libkaapic.so:../.libs/libgomp.so"
+    export LD_PRELOAD="$KAAPI_INSTALL_PATH/lib/libkaapic.so:$KAAPI_INSTALL_PATH/lib/libgomp.so"
 fi
 
 testname=$1
