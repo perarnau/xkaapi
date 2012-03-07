64687d6313550f761427aa202905609e45c91cea
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2012-03-07 16:53:40 +0100

    [build] add generated poti_config file
    This file is created by poti when compiling the whole poti library.
    We just imported it along the other poti files we need.

---
+++ bin/kaapi_event_reader/poti/src/poti_config.h
@@ -0,0 +1,23 @@
+/*
+    This file is part of Poti
+
+    Poti is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Poti is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser Public License for more details.
+
+    You should have received a copy of the GNU Lesser Public License
+    along with Poti. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _POTI_CONFIG_H
+#define _POTI_CONFIG_H
+
+#define POTI_GITVERSION "52e8c11 fix indentation"
+#define POTI_GITDATE "2012-02-27 12:56:54 +0100"
+
+#endif
