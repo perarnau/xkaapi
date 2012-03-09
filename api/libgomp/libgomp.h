3948c33c9764650ebfc8c239647b6ae4ac947a1c
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-03-09 14:01:17 +0100

    Put OMP runtime function in .h

--- api/libgomp/libgomp.h
+++ api/libgomp/libgomp.h
@@ -291,4 +291,48 @@ extern void *GOMP_single_copy_start (void);
 extern void GOMP_single_copy_end (void *);
 
 
+/* function from the runtime support of OMP */
+typedef enum omp_sched_t {
+    omp_sched_static = 1,
+    omp_sched_dynamic = 2,
+    omp_sched_guided = 3,
+    omp_sched_auto = 4
+} omp_sched_t;
+
+typedef kaapi_atomic8_t omp_lock_t;
+typedef kaapi_atomic32_t omp_nest_lock_t;
+
+extern void omp_set_num_threads (int n);
+extern int omp_get_num_threads (void);
+extern int omp_get_thread_num (void);
+extern int omp_get_max_threads (void);
+extern int omp_get_num_procs (void);
+extern int omp_in_parallel(void);
+extern void omp_set_dynamic(int dynamic_threads );
+extern int omp_get_dynamic(void);
+extern void omp_set_nested(int nested);
+extern int omp_get_nested(void);
+extern void omp_set_schedule(omp_sched_t kind, int modifier );
+extern void omp_get_schedule(omp_sched_t * kind, int * modifier );
+extern int omp_get_thread_limit(void);
+extern void omp_set_max_active_levels (int max_levels );
+extern int omp_get_max_active_levels(void);
+extern int omp_get_level(void);
+extern int  omp_get_ancestor_thread_num(int level);
+extern int omp_get_team_size(int level);
+extern int omp_get_active_level(void);
+extern int omp_in_final(void);
+extern double omp_get_wtime(void);
+extern double omp_get_wtick(void);
+extern void omp_init_lock(omp_lock_t *lock);
+extern void omp_destroy_lock(omp_lock_t *lock);
+extern void omp_set_lock(omp_lock_t *lock);
+extern void omp_unset_lock(omp_lock_t *lock);
+extern int omp_test_lock(omp_lock_t *lock);
+extern void omp_init_nest_lock(omp_nest_lock_t *lock);
+extern void omp_destroy_nest_lock(omp_nest_lock_t *lock);
+extern void omp_set_nest_lock(omp_nest_lock_t *lock);
+extern void omp_unset_nest_lock(omp_nest_lock_t *lock);
+extern int omp_test_nest_lock(omp_nest_lock_t *lock);
+
 #endif // #ifndef _KAAPI_LIBGOMP_
