7e591f7095e04f1d4e365dd9e153defaa738194d
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-03-12 00:19:06 +0100

    Update rts

--- api/libgomp/libomprt.c
+++ api/libgomp/libomprt.c
@@ -112,6 +112,8 @@ omp_get_dynamic(void)
 void 
 omp_set_nested(int nested __attribute__((unused)))
 {
+  kompctxt_t* ctxt = komp_get_ctxt();
+  ctxt->icv.nestedparallel = (nested !=0);
 }
 
 /*
@@ -119,7 +121,8 @@ omp_set_nested(int nested __attribute__((unused)))
 int 
 omp_get_nested(void)
 {
-  return 0; /* not yet implemented */
+  kompctxt_t* ctxt = komp_get_ctxt();
+  return (ctxt->icv.nestedparallel !=0);
 }
 
 /*
