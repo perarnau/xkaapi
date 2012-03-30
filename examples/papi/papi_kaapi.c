b82dae0604a8836bdd47cad1744e6506e47f9f3c
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-03-30 12:27:28 +0200

    Update papi example

--- examples/papi/papi_kaapi.c
+++ examples/papi/papi_kaapi.c
@@ -46,164 +46,17 @@
    see examples/for_each for general code information
  */
 
-#include "kaapi.h"
+#include "kaapic.h"
 #include <string.h>
 #include <math.h>
-
-typedef struct work
-{
-  kaapi_workqueue_t cr;
-  void (*op)(double*);
-  double* array;
-} work_t;
-
-typedef struct thief_work_t {
-  void (*op)(double*);
-  double* beg;
-  double* end;
-} thief_work_t;
-
-
-/* fwd decl */
-static void thief_entrypoint(void*, kaapi_thread_t*);
-
-
-/* parallel work splitter */
-static int splitter
-(
- kaapi_stealcontext_t* sc, 
- int nreq, kaapi_request_t* req, 
- void* args
-)
-{
-  /* victim work */
-  work_t* const vw = (work_t*)args;
-  
-  /* stolen range */
-  kaapi_workqueue_index_t i, j;
-  kaapi_workqueue_index_t range_size;
-  
-  /* reply count */
-  int nrep = 0;
-  
-  /* size per request */
-  kaapi_workqueue_index_t unit_size;
-  
-redo_steal:
-  /* do not steal if range size <= PAR_GRAIN */
-#define CONFIG_PAR_GRAIN 128
-  range_size = kaapi_workqueue_size(&vw->cr);
-  if (range_size <= CONFIG_PAR_GRAIN)
-    return 0;
-  
-  /* how much per req */
-  unit_size = range_size / (nreq + 1);
-  if (unit_size == 0)
-  {
-    nreq = (range_size / CONFIG_PAR_GRAIN) - 1;
-    unit_size = CONFIG_PAR_GRAIN;
-  }
-  
-  /* perform the actual steal. if the range
-   changed size in between, redo the steal
-   */
-  if (kaapi_workqueue_steal(&vw->cr, &i, &j, nreq * unit_size))
-    goto redo_steal;
-  
-  for (; nreq; --nreq, ++req, ++nrep, j -= unit_size)
-  {
-    /* thief work: not adaptive result because no preemption is used here  */
-    thief_work_t* const tw = kaapi_reply_init_adaptive_task
-    ( sc, req, thief_entrypoint, sizeof(thief_work_t), 0 );
-    tw->op  = vw->op;
-    tw->beg = vw->array+j-unit_size;
-    tw->end = vw->array+j;
-    
-    kaapi_reply_push_adaptive_task(sc, req);
-  }
-  
-  return nrep;
-}
-
-
-/** seq work extractor 
- */
-static int extract_seq(work_t* w, double** pos, double** end)
-{
-  /* extract from range beginning */
-  kaapi_workqueue_index_t i, j;
-  
-#define CONFIG_SEQ_GRAIN 256
-  if (kaapi_workqueue_pop(&w->cr, &i, &j, CONFIG_SEQ_GRAIN)) return 1;
-  
-  *pos = w->array + i;
-  *end = w->array + j;
-  
-  return 0; /* success */
-}
-
-
-/** thief entrypoint 
- */
-static void inline common_entry
-(double* beg, double* end, void (*op)(double*))
-{
-  for (; beg != end; ++beg) op(beg);
-}
-
-static void thief_entrypoint(void* args, kaapi_thread_t* thread)
-{
-  /* process the work */
-  thief_work_t* const thief_work = (thief_work_t*)args;
-  
-  /* range to process */
-  double* const beg = thief_work->beg;
-  double* const end = thief_work->end;
-  
-  /* apply w->op foreach item in [pos, end[ */
-  common_entry(beg, end, thief_work->op);
-}
-
-
-/* For each main function */
-static void for_each( double* array, size_t size, void (*op)(double*) )
-{
-  const int sc_flags = KAAPI_SC_CONCURRENT | KAAPI_SC_NOPREEMPTION;
-
-  kaapi_thread_t* thread;
-  kaapi_stealcontext_t* sc;
-  work_t  work;
-  double* pos;
-  double* end;
-  
-  /* get the self thread */
-  thread = kaapi_self_thread();
-  
-  /* initialize work */
-  kaapi_workqueue_init(&work.cr, 0, (kaapi_workqueue_index_t)size);
-  work.op    = op;
-  work.array = array;
-  
-  /* push an adaptive task */
-  sc = kaapi_task_begin_adaptive
-    (thread, sc_flags, splitter, &work);
-  
-  /* while there is sequential work to do*/
-  while (!extract_seq(&work, &pos, &end))
-    common_entry(pos, end, op);
-  
-  kaapi_task_end_adaptive(thread, sc);
-
-  /* wait for thieves */
-  kaapi_sched_sync();
-}
 
 
 /**
  */
-static void apply_cos( double* v )
+static void apply_cos( int i, int j, int tid, double* v )
 {
-  *v += cos(*v);
+  for (int k=i; k<j; ++k)
+    array[k] += cos(array[k]);
 }
 
 
@@ -224,7 +77,7 @@ int main(int ac, char** av)
   static double array[ITEM_COUNT];
   
   /* initialize the runtime */
-  kaapi_init(1, &ac, &av);
+  kaapic_init(KAAPIC_START_ONLY_MAIN);
 
   /* create performance counter id set. use the
      KAAPI_PERF_PAPIES=a,b,c environ variable to
@@ -243,7 +96,7 @@ int main(int ac, char** av)
       array[i] = 0.f;
 
     t0 = kaapi_get_elapsedns();
-    for_each( array, ITEM_COUNT, apply_cos );
+    kaapic_for_each( 0, ITEM_COUNT, 0, 1, apply_cos, array );
     t1 = kaapi_get_elapsedns();
     sum += (t1-t0)/1000; /* ms */
 
@@ -265,7 +118,7 @@ int main(int ac, char** av)
 	 counters[0], counters[1], counters[2]);
 
   /* finalize the runtime */
-  kaapi_finalize();
+  kaapic_finalize();
   
   return 0;
 }
