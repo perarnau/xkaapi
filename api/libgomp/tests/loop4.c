6ee1e6a93421909a41e085b52701c9b23ac50c94
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-05-24 11:22:16 +0200

    Update test loop4

--- api/libgomp/tests/loop4.c
+++ api/libgomp/tests/loop4.c
@@ -6,10 +6,11 @@ main (int argc, char **argv)
 {
   int i;
   
-  omp_set_datadistribution_bloccyclic( 32, 64 );
+  omp_set_datadistribution_bloccyclic( 32, 4 );
 #pragma omp parallel for schedule(dynamic)
-  for (i=0; i<123; ++i)
+  for (i=0; i<128; ++i)
   {
+    printf("%i\n",i);
   }
 
   return 0;
