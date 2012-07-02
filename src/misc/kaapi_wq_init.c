c6cd41b5488b93f32e0675a8f1003a7cd0bcf2de
Thierry <thierry@crowh.imag.fr>
2012-07-02 11:15:45 +0200

    Update to add workqueue init with kproc ident as parameter

--- src/misc/kaapi_wq_init.c
+++ src/misc/kaapi_wq_init.c
@@ -66,28 +66,3 @@ int kaapi_workqueue_init(
   kwq->lock = &kproc->lock;
   return 0;
 }
-
-
-/** 
-*/
-int kaapi_workqueue_init_with_lock( 
-    kaapi_workqueue_t*      kwq, 
-    kaapi_workqueue_index_t b, 
-    kaapi_workqueue_index_t e, 
-    kaapi_lock_t*           thelock 
-)
-{
-  kaapi_mem_barrier();
-#if defined(__i386__)||defined(__x86_64)||defined(__powerpc64__)||defined(__powerpc__)||defined(__ppc__)||defined(__arm__)
-  kaapi_assert_debug( (((unsigned long)&kwq->rep.li.beg) & (sizeof(kaapi_workqueue_index_t)-1)) == 0 ); 
-  kaapi_assert_debug( (((unsigned long)&kwq->rep.li.end) & (sizeof(kaapi_workqueue_index_t)-1)) == 0 );
-#else
-#  error "May be alignment constraints exit to garantee atomic read write"
-#endif
-  kaapi_assert_debug( b <= e );
-  kaapi_assert_debug( thelock != 0 );
-  kwq->rep.li.beg  = b;
-  kwq->rep.li.end  = e;
-  kwq->lock = thelock;
-  return 0;
-}
