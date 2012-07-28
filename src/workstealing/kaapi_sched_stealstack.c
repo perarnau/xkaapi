e071cb17c6cdb64a63783b909677b3655ab3a426
Thierry Gautier <thierry@inrialpes.fr>
2012-07-28 20:28:38 +0200

    Update: should steal in stack, especially if no distribution is used

--- src/workstealing/kaapi_sched_stealstack.c
+++ src/workstealing/kaapi_sched_stealstack.c
@@ -91,7 +91,7 @@ int kaapi_sched_stealstack
       kaapi_sched_stealframe( thread, top_frame, &access_to_gd, lrequests, lrrange );
     } 
 #if 1 // To disable steal in ready list, in order to let only remote push
-    else if (thread->stack.proc->kid ==0)
+    else /* if (thread->stack.proc->kid ==0)  */
       kaapi_sched_stealtasklist( thread, top_frame->tasklist, lrequests, lrrange );
 #endif
   }
