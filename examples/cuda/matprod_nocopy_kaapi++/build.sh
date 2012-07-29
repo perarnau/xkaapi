1916bf355536253c71b272c48ade00ca4b04878f
Joao Lima <Joao.Lima@imag.fr>
2012-07-29 19:54:04 +0200

    Local modification for my mac...

--- examples/cuda/matprod_nocopy_kaapi++/build.sh
+++ examples/cuda/matprod_nocopy_kaapi++/build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-SCRATCH=$SCRATCH
-XKAAPIDIR=$SCRATCH/install/xkaapi/nocopy
+SCRATCH=/tmp
+XKAAPIDIR=/tmp/xkaapi
+
+CXX=g++-mp-4.6
+#CXX=g++
 
 function do_test() {
     eval var=\$$1
     if [ "x$var" = "x" ]
     then
-	echo "ERROR: $2"
-	exit 0
+        echo "ERROR: $2"
+        exit 0
     fi
 }
+
+CBLAS_CFLAGS="-I/usr/local/atlas/include"
+CBLAS_LDFLAGS="/usr/local/atlas/lib/libcblas.a /usr/local/atlas/lib/liblapack.a /usr/local/atlas/lib/libatlas.a"
+LAPACKE_CFLAGS="-I/usr/local/include"
+LAPACKE_LDFLAGS="-L/usr/local/lib -llapacke -llapack"
 
 do_test "CBLAS_CFLAGS" "No CBLAS_CFLAGS found."
 do_test "CBLAS_LDFLAGS" "No CBLAS_LDFLAGS found."
@@ -18,19 +26,19 @@ do_test "CBLAS_LDFLAGS" "No CBLAS_LDFLAGS found."
 #do_test "LAPACK_LDFLAGS" "No LAPACK_LDFLAGS found."
 do_test "LAPACKE_CFLAGS" "No LAPACKE_CFLAGS found."
 do_test "LAPACKE_LDFLAGS" "No LAPACKE_LDFLAGS found."
-do_test "CUDA_CFLAGS" "No CUDA_CFLAGS found."
-do_test "CUDA_LDFLAGS" "No CUDA_LDFLAGS found."
+#do_test "CUDA_CFLAGS" "No CUDA_CFLAGS found."
+#do_test "CUDA_LDFLAGS" "No CUDA_LDFLAGS found."
 
 CFLAGS="-DKAAPI_DEBUG=0 -DKAAPI_NDEBUG=1
 -DCONFIG_USE_DOUBLE=1 -I$XKAAPIDIR/include"
 LDFLAGS="-L$XKAAPIDIR/lib -lkaapi -lkaapi++"
 
-CUDA_CFLAGS="-DCONFIG_USE_CUDA=1 $CUDA_CFLAGS"
-
+CUDA_CFLAGS="-I/usr/local/cuda/include -DCONFIG_USE_CUDA=1 $CUDA_CFLAGS"
 CUBLAS_CFLAGS="-DCONFIG_USE_CUBLAS=1"
-CUBLAS_LDFLAGS="-lcublas"
+CUBLAS_LDFLAGS="-L/usr/local/cuda/lib -lcublas -lcuda -lcudart"
 
-g++ -g -Wall \
+#g++ -g -Wall
+$CXX -O3 -Wall \
     $CFLAGS \
     $CUDA_CFLAGS \
     $CBLAS_CFLAGS \
