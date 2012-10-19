49b890d3b0f5e33dae68032a77d9e89b9d303ab4
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-10-19 09:56:13 +0200

    Force init to 0 for _sync.

---
+++ api/libgomp/testsuite/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  xkaapi
+//
+//  Created by thierry gautier on 13/10/12.
+//  Copyright (c) 2012 INRIA. All rights reserved.
+//
+
+#ifndef xkaapi_Header_h
+#define xkaapi_Header_h
+
+
+
+#endif
