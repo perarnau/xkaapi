0360d1430fbefd66ed9bbc50a17147ac4922cd55
Thierry Gautier <Thierry.Gautier@imag.fr>
2012-11-26 16:00:52 +0100

    [add] - new tests

--- tests/c++/test_nqueens_stackpointer.cpp
+++ tests/c++/test_nqueens_stackpointer.cpp
@@ -4,6 +4,36 @@
 
 #include "kaapi++"
 
+/* last entry i in solution[i] */
+#define MAX_CHESSBOARD_SIZE  23 
+
+long solution[] = {
+0, /* chessboard size = 0 */
+1,
+0,
+0,
+2,
+10,
+4,
+40,
+92,
+352,
+724,
+2680,
+14200,
+73712,
+365596,
+2279184,
+14772512,
+95815104,
+666090624,
+4968057848,
+39029188884,
+314666222712,
+2691008701644,
+24233937684440
+};
+
 #define THRESHOLD 5
 /* */
 struct TaskSolve : public ka::Task<5>::Signature<
@@ -56,5 +86,9 @@ void doit::operator()(int argc, char** argv )
   ka::Sync();
 
   std::cout << "Total number of solutions: " << count << std::endl;
+  if (n <MAX_CHESSBOARD_SIZE)
+  {
+    kaapi_assert( count == solution[n] );
+  }
 }
 
