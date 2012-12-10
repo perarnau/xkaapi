029c4b6e36e7c5a1802d14a14e678217e1dcb6c6
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2012-12-11 00:08:13 +0100

    [tests] add a test for the fortran API
    The test use the generated include files
    The .F extension (instead of .f) tells automake to run the preprocessor

--- tests/api-kaapif/spawn_v-args_fu.f
+++ tests/api-kaapif/spawn_v-args_fu.f
@@ -1,10 +1,14 @@
-      subroutine fu(array, size)
+      subroutine fu(array, size, cinit, chunk, f1, f2)
 
       ! process array[0, size[
       ! process array[1, size]
 
       real*8 array(*)
       integer*4 size
+      byte cinit
+      integer*4 chunk
+      real*4 f1
+      real*8 f2
       integer*4 i
 
       ! integer*4 tid
@@ -12,7 +16,7 @@
       ! write(*, *) tid, 'FU', size
 
       do i = 1, size
-         array(i) = 42
+         array(i) = cinit+f1*chunk+f2*i
       end do
 
       return
